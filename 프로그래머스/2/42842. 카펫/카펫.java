df1a241f6419ffcb269683b1cb0b57c0996b3a12
hessepark <78264208+hessepark@users.noreply.github.com>
2024-11-30 12:02:26 +0900

    [level 2] Title: 카펫, Time: 0.06 ms, Memory: 87.2 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -10,9 +10,9 @@ class Solution {
         int a=-1;
         int b=-1;
         
-        for(int i=1;i<=Math.sqrt(result);i++){
+        for(int i=2;i<=Math.sqrt(result);i++){
             if(result%i==0){
-                if((i-2)*(result/i-2)==yellow){ //이 조건 써주지 않으면 테스트케이스 4,6,7 에러
+                if((i-2)*(result/i-2)==yellow){ //이 조건 써주지 않으면 테스트케이스 4,6,7 에러남
                     a=i;
                     b=result/i;
                 }
