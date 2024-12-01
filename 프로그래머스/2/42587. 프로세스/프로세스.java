e7188e47d9889455fb66a4859e33cff39130ef07
hessepark <78264208+hessepark@users.noreply.github.com>
2024-12-01 10:58:43 +0900

    [level 2] Title: 프로세스, Time: 1.58 ms, Memory: 75.1 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.java
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -1,56 +1,38 @@
-import java.util.ArrayDeque;
-import java.util.Iterator;
-
-class process {
-	int idx;
-	int num;
-
-	public process(int idx, int num) {
-		this.idx = idx;
-		this.num = num;
-	}
-}
+import java.io.*;
+import java.util.*;
 
 class Solution {
-	public static int solution(int[] priorities, int location) {
-		int answer = 0;
+    public int solution(int[] priorities, int location) {
+        
+         PriorityQueue<Integer>q = new PriorityQueue<>((o1,o2)->Integer.compare(o2,o1));
+        
+        for(int i=0;i<priorities.length;i++){
+            q.add(priorities[i]);
+        }
+        
+        //System.out.println(q);
+        
+        int idx=0;
+        int num=q.poll();
+        
+        System.out.println(num);
+        
+        while(true){
+            
+            for(int i=0;i<priorities.length;i++){
+                if(priorities[i]==num){
+                    idx++;
+                    
+                    if(location==i){
+                        return idx;
+                    }
+                    num=q.poll(); //같은 거 발견했을 때만 poll
+                }
+               
+            }
+        
+    
 
-		ArrayDeque<process> q = new ArrayDeque<>();
-
-		for (int i = 0; i < priorities.length; i++) {
-			process pro = new process(i, priorities[i]);
-			q.add(pro);
-		}
-
-		int cnt = 0;
-
-		while (true) {
-			int max = Integer.MIN_VALUE;
-			Iterator<process> it = q.iterator();
-			while (it.hasNext()) {
-				max = Math.max(max, it.next().num);
-			}
-
-			while (q.peekFirst().num != max) {
-				process pro = q.poll();
-				q.add(pro);
-			}
-
-			process pro = q.poll();
-			cnt++;
-			if (pro.idx == location) {
-				break;
-			}
-
-		}
-
-		answer = cnt;
-
-		return answer;
-	}
-	// public static void main(String[] args) {
-	// 	int priorities[]= {1,1,9,1,1,1};
-	// 	int location=0;
-	// 	System.out.println(solution(priorities,location));
-	// }
+    }
+}
 }
