94482ddb478beba9960b01d5d765a3c74f156d63
hessepark <78264208+hessepark@users.noreply.github.com>
2024-12-04 11:08:59 +0900

    [level 2] Title: 카펫, Time: 0.07 ms, Memory: 79.4 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -13,14 +13,14 @@ class Solution {
         for(int i=3;i<=Math.sqrt(result);i++){ //최소 3이상이어야 노란색이 들어갈 공간이 남
             if(result%i==0){
                 if((i-2)*(result/i-2)==yellow){ //이 조건 써주지 않으면 테스트케이스 4,6,7 에러남
-                    a=i;
-                    b=result/i;
+                    a=result/i;
+                    b=i;
                 }
             }
         }
         
-        answer[0]=Math.max(a,b);
-        answer[1]=Math.min(a,b);
+        answer[0]=a;
+        answer[1]=b;
         
         return answer;
     }
