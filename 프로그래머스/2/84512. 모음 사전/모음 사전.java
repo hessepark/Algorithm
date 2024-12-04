e34cfb4470adc6af75be32d92a8a0dec7b12ad76
hessepark <78264208+hessepark@users.noreply.github.com>
2024-12-04 14:35:44 +0900

    [level 2] Title: 모음 사전, Time: 30.64 ms, Memory: 92.3 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.java
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.java
@@ -14,7 +14,7 @@ class Solution {
         cnt = 0;
         found = false;
         
-        make("",0,word); // 깊이
+        make("",word); // 깊이
         
         //ans=cnt;
         
@@ -22,7 +22,7 @@ class Solution {
     
     }
     
-    public static void make(String cur,int depth,String word){
+    public static void make(String cur,String word){
         
         if(cur.equals(word)){
             ans=cnt;
@@ -30,24 +30,21 @@ class Solution {
             return;
         }
         
-        if(depth==5){
-            return;
-        }
         
         for(int i=0;i<5;i++){
 
            
             
             //System.out.println(cur+arr[i]);
-           // if(cur.length()<5){ //깊이를 cur.length()로 대신한 거임
+            if(cur.length()<5){ //깊이를 cur.length()로 대신한 거임
            
                 cnt++;
-                make(cur+arr[i],depth+1,word);
+                make(cur+arr[i],word);
              if(found){
                 return;
             }
             
-           // };
+            };
         }
         
         
