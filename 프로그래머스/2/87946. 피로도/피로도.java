d5564fb866649728d8fa242a8f0ef0480c36fdf6
hessepark <78264208+hessepark@users.noreply.github.com>
2024-12-04 13:14:04 +0900

    [level 2] Title: 피로도, Time: 2.80 ms, Memory: 90.9 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -1,43 +1,36 @@
-import java.util.*;
-import java.io.*;
-
-//순열
-
-
 class Solution {
     
+    public static boolean isVisited[];
+    public static int max;
     
-public static boolean isVisited[];
-public static int max;
-    
-    public int solution(int k, int[][] dungeons) {   
-        max=Integer.MIN_VALUE;
+    public int solution(int k, int[][] dungeons) {
+      
+        max = Integer.MIN_VALUE;
+        isVisited=new boolean[dungeons.length+1];
         
-        isVisited=new boolean[dungeons.length];
-        
-        
-        //for(int i=0;i<dungeons.length;i++) { //모든 던전을 출발지로 삼음
-            perm(k,dungeons,0); //현재 피로도 ,던전, 위치, 깊이 
-        //}
+        perm(k,0,dungeons);//피로도,깊이
         
         return max;
     }
     
-    public static void perm(int current,int[][] dungeons,int cnt){
+    public static void perm(int cur,int depth,int[][] dungeons) {
         
-
-         
-        max=Math.max(cnt,max); //최대 갱신
+        max=Math.max(max,depth);
+        
+        if(depth==dungeons.length){
+            return;
+        }
         
         for(int i=0;i<dungeons.length;i++){
-            if(!isVisited[i]&&!(current<dungeons[i][0])){
+            if(cur>=dungeons[i][0]&&!isVisited[i]){
                 isVisited[i]=true;
-                perm(current-dungeons[i][1],dungeons,cnt+1);
+               //System.out.println(i);
+                perm(cur-dungeons[i][1],depth+1,dungeons);
                 isVisited[i]=false;
             }
         }
         
-    
+        
     }
     
 }
