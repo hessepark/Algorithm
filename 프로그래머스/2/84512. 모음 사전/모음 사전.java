849a11febe7bb011165656d54730bb2fa849ce9b
hessepark <78264208+hessepark@users.noreply.github.com>
2024-12-04 14:16:58 +0900

    [level 2] Title: 모음 사전, Time: 17.87 ms, Memory: 96.2 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.java
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.java
@@ -1,60 +1,40 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
-class Solution { // 0,00,...00000,00001 순서대로 만들어놓고 계산
-    
-    public static HashMap<String,Integer> map;
+class Solution {
+   
     public static int cnt;
+    public static int ans;
+    public static char arr[]={'A','E','I','O','U'};
     
     public int solution(String word) {
-        int answer = 0;
         
-        cnt=1;
-        map = new HashMap<>();
+        ans=0;
+        cnt = 0;
         
-        make("",0);
+        make("",word); // 깊이
         
-        StringBuilder sb = new StringBuilder();
+        //ans=cnt;
         
-        for(int i=0;i<word.length();i++) {
-            if(word.charAt(i)=='A'){
-                sb.append(0);
-            }
-            else if(word.charAt(i)=='E'){
-                sb.append(1);
-            }
-            else if(word.charAt(i)=='I'){
-                sb.append(2);
-            }
-            else if(word.charAt(i)=='O'){
-                sb.append(3);
-            }
-            else{
-                sb.append(4);
-            }
-        }
-        
-        //System.out.println(sb);
-        
-        answer = map.get(sb.toString());
-        
-        return answer;
+        return ans;
+    
     }
     
-    public static void make(String cur,int depth){
+    public static void make(String cur,String word){
         
-        if(depth==5){
+        if(cur.equals(word)){
+            ans=cnt;
             return;
         }
         
         for(int i=0;i<5;i++){
-            map.put(cur+Integer.toString(i),cnt);
-            //System.out.println(cur+Integer.toString(i));
-            cnt++;
-            make(cur+Integer.toString(i),depth+1);
+            //System.out.println(cur+arr[i]);
+            if(cur.length()<5){
+                cnt++;
+                make(cur+arr[i],word);
+            };
         }
         
         
-    
     }
 }
