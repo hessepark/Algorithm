03b6c336e2578b152b18a985a2d9f721e9abf738
hessepark <78264208+hessepark@users.noreply.github.com>
2024-12-04 09:45:15 +0900

    [Silver IV] Title: 덱, Time: 260 ms, Memory: 20400 KB -BaekjoonHub

---
+++ 백준/Silver/10866. 덱/덱.java
@@ -0,0 +1,59 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Deque;
+import java.util.LinkedList;
+
+class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		int N = Integer.parseInt(br.readLine());
+
+		Deque<Integer> q = new LinkedList<>();
+
+		while (N-- > 0) {
+
+			String cmd[] = br.readLine().split(" ");
+
+			if (cmd[0].equals("push_front")) {
+				q.addFirst(Integer.parseInt(cmd[1]));
+			}
+
+			else if (cmd[0].equals("push_back")) {
+				q.addLast(Integer.parseInt(cmd[1]));
+			}
+
+			else if (cmd[0].equals("pop_front")) {
+				bw.write(q.isEmpty() ? "-1\n" : q.removeFirst() + "\n");
+			}
+
+			else if (cmd[0].equals("pop_back")) {
+				bw.write(q.isEmpty() ? "-1\n" : q.removeLast() + "\n");
+			}
+
+			else if (cmd[0].equals("size")) {
+				bw.write(q.size() + "\n");
+			}
+
+			else if (cmd[0].equals("empty")) {
+				bw.write(q.isEmpty() ? "1\n" : "0\n");
+			}
+
+			else if (cmd[0].equals("front")) {
+				bw.write(q.isEmpty() ? "-1\n" : q.getFirst() + "\n");
+			}
+
+			else if (cmd[0].equals("back")) {
+				bw.write(q.isEmpty() ? "-1\n" : q.peekLast() + "\n");
+			}
+
+		}
+
+		bw.flush();
+
+	}
+}
