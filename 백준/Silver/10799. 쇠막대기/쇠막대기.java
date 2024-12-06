365d32c103e072c4669752ccffafc235d35fd736
hessepark <78264208+hessepark@users.noreply.github.com>
2024-12-06 16:20:41 +0900

    [Silver II] Title: 쇠막대기, Time: 280 ms, Memory: 22560 KB -BaekjoonHub

--- 백준/Silver/10799. 쇠막대기/쇠막대기.java
+++ 백준/Silver/10799. 쇠막대기/쇠막대기.java
@@ -1,40 +1,32 @@
+import java.util.ArrayDeque;
 import java.util.Scanner;
 
 class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-
-		String str = sc.next();
-
-		int N = str.length();
-
-		char arr[] = new char[N + 1];
-
-		System.arraycopy(str.toCharArray(), 0, arr, 1, N);
-
-		int lasers[] = new int[N + 1];
-
-		for (int i = 1; i <= N; i++) {
-			lasers[i] = lasers[i - 1];
-			if (arr[i] == '(' && arr[i + 1] == ')') {
-				lasers[i]++;
+		
+		char str[]=sc.next().toCharArray();
+		
+		ArrayDeque<Character>q = new ArrayDeque<>();
+		
+		int result=0;
+		
+		for(int i=0;i<str.length;i++) {
+			if(str[i]=='(') {
+				q.add(str[i]);
+			}
+			else {
+				q.pollLast();
+				if(str[i-1]=='(') {
+					result+=q.size();
+				}
+				else {
+					result++;
+				}
 			}
 		}
-
-		int[] openIndex = new int[N + 1];
-		int topIndex = -1;
-		int ans = 0;
-
-		for (int i = 1; i <= N; i++) {
-			if (arr[i] == '(' && arr[i + 1] != ')') {
-				openIndex[++topIndex] = i;
-			} else if (arr[i] == ')' && arr[i - 1] != '(') {
-				int open = openIndex[topIndex--];
-				ans += lasers[i] - lasers[open - 1] + 1;
-			}
-		}
-
-		System.out.println(ans);
-
+		
+		System.out.println(result);
+		
 	}
 }
