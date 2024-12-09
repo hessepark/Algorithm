43009f0dca4f97ff030dfcd6e90de30aeaefb8ef
hessepark <78264208+hessepark@users.noreply.github.com>
2024-12-09 10:14:06 +0900

    [Silver I] Title: 외계인의 기타 연주, Time: 1672 ms, Memory: 402356 KB -BaekjoonHub

--- 백준/Silver/2841. 외계인의 기타 연주/외계인의 기타 연주.java
+++ 백준/Silver/2841. 외계인의 기타 연주/외계인의 기타 연주.java
@@ -1,14 +1,14 @@
 import java.util.ArrayDeque;
 import java.util.Scanner;
 
-class Main {
+class Main { 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 
 		int n = sc.nextInt();
 		int p = sc.nextInt();
 
-		ArrayDeque<Integer> q[] = new ArrayDeque[n + 1];
+		ArrayDeque<Integer> q[] = new ArrayDeque[n + 1];//큐를 독립적으로 만들어서 차근차근 풀면 된다.
 
 		for (int i = 1; i < n + 1; i++) {
 			q[i] = new ArrayDeque<>();
