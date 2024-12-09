2c4303536f4bbe6be370dec230ea8e81670aa3ad
hessepark <78264208+hessepark@users.noreply.github.com>
2024-12-09 10:05:50 +0900

    [Silver I] Title: 외계인의 기타 연주, Time: 1696 ms, Memory: 404868 KB -BaekjoonHub

---
+++ 백준/Silver/2841. 외계인의 기타 연주/외계인의 기타 연주.java
@@ -0,0 +1,41 @@
+import java.util.ArrayDeque;
+import java.util.Scanner;
+
+class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int n = sc.nextInt();
+		int p = sc.nextInt();
+
+		ArrayDeque<Integer> q[] = new ArrayDeque[n + 1];
+
+		for (int i = 1; i < n + 1; i++) {
+			q[i] = new ArrayDeque<>();
+		}
+
+		int cnt = 0;
+
+		for (int i = 0; i < n; i++) {
+			int line = sc.nextInt();
+			int pret = sc.nextInt();
+
+			while (!q[line].isEmpty() && q[line].peekLast() > pret) {
+				q[line].pollLast();
+				cnt++;
+			}
+
+			if (q[line].isEmpty() || q[line].peekLast() != pret) {
+				q[line].add(pret);
+				cnt++;
+			}
+
+			//System.out.println(q[line]);
+			//System.out.println(cnt);
+
+		}
+
+		System.out.println(cnt);
+
+	}
+}
