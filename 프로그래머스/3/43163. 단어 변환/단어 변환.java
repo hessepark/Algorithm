29f551b67949e63338bfeaa4a35c6808b7075964
hessepark <78264208+hessepark@users.noreply.github.com>
2024-12-11 14:27:43 +0900

    [level 3] Title: 단어 변환, Time: 0.87 ms, Memory: 72.3 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -1,49 +1,70 @@
-//안 가본 곳이고 한 글자만 다르면 계속 들어간다
-//들어가다가 target이랑 같으면 return;
-//for문 끝나면 갔던곳 원복
+//bfs로 번지다가 도착하면 바로 종료
 
 import java.util.*;
 import java.io.*;
 
-
 class Solution {
     
     public static int ans;
-    public static boolean[] isVisited;
+    public static int visited[];
     
     public int solution(String begin, String target, String[] words) {
-        ans=0;
-        isVisited =new boolean[words.length];
+        ans = 0;
         
-        dfs(begin,target,words,0);
+        visited=new int[words.length];
+        
+        bfs(begin,target,words);
         
         return ans;
     }
     
-    public static void dfs(String cur, String target,String[] words,int cnt){
+    public static void bfs(String cur,String target,String[] words){
         
-        if(cur.equals(target)){
-            ans=cnt;
-            return;
-        }
-        
-        //깊이가 끝까지 갔으면?
-        
-        for(int i=0;i<words.length;i++){
-            if(!isVisited[i]){
-                if(compare(cur,words[i])){ //함수 충족하면
-                    isVisited[i]=true;
-                    dfs(words[i],target,words,cnt+1);
-                    isVisited[i]=false;
+        ArrayDeque<Integer>q = new ArrayDeque<>();
+        //출발 가능한 지점을 q에 다 넣는다
+         for(int i=0;i<words.length;i++){
+            if(check(words[i],cur)){
+                q.add(i);
+                visited[i]=1;
+               System.out.println(i);
+                
+                  if(words[i].equals(target)){ //ans가 처음으로 나오면
+                    ans=visited[i];
+                    break;
                 }
+                
             }
         }
         
+        if(ans!=0) return;
         
+        //System.out.println(q);
+        
+        while(!q.isEmpty()){
+            int now = q.poll();
+            
+            for(int i=0;i<words.length;i++){
+                if(visited[i]==0&&check(words[now],words[i])){
+                    visited[i]=visited[now]+1;
+                    q.add(i);
+                    System.out.println(i);
+                     if(words[i].equals(target)){ //ans가 처음으로 나오면
+                    ans=visited[i];
+                    break;
+                }
+                }
+               
+            }
+            if(ans!=0){
+                break;
+            }
+            
+        }
+        
+       
     }
     
-    public static boolean compare(String a,String b){
-        
+    public static boolean check(String a,String b){
         int cnt=0;
         
         for(int i=0;i<a.length();i++){
