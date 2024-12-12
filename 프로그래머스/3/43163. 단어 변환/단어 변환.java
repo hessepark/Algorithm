f9a8bc758f60fbf4372ea7e81705cd09fa84640a
hessepark <78264208+hessepark@users.noreply.github.com>
2024-12-12 23:43:09 +0900

    [level 3] Title: 단어 변환, Time: 0.67 ms, Memory: 87 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -4,53 +4,52 @@ import java.io.*;
 class Data {
     String word;
     int cnt;
-    
-    public Data(String word, int cnt){
+    public Data(String word,int cnt){
         this.word=word;
         this.cnt=cnt;
     }
-    
 }
 
 class Solution {
-    
-    public static int ans;
+        
     public static boolean isVisited[];
+    public static int ans;
     
     public int solution(String begin, String target, String[] words) {
+
         ans = 0;
         
-        isVisited=new boolean[words.length];
+        isVisited = new boolean[words.length];
         
-        bfs(begin,target,words,0);
-       
+        bfs(begin,target,words);
         
         return ans;
         
-        
     }
     
-    public static void bfs(String begin,String target,String[]words,int cnt){
-        
-        ArrayDeque<Data>q = new ArrayDeque<>();
+    public static void bfs(String begin,String target,String[] words){
+        ArrayDeque<Data> q = new ArrayDeque<>();
         q.add(new Data(begin,0));
         
         while(!q.isEmpty()){
             Data now = q.poll();
+            
             for(int i=0;i<words.length;i++){
                 if(!isVisited[i]&&check(now.word,words[i])){
-                    q.add(new Data(words[i],now.cnt+1));
                     isVisited[i]=true;
-                  if(words[i].equals(target)){
+                    q.add(new Data(words[i],now.cnt+1));
+                    if(words[i].equals(target)){
                         ans=now.cnt+1;
-                        // System.out.println(i);
-                        //System.out.println(now.word);
-                      //return 지움
+                        return;
                     }
                 }
-                  
+                
             }
+            
+            
         }
+        
+        
         
     }
     
@@ -58,15 +57,17 @@ class Solution {
         int cnt=0;
         
         for(int i=0;i<a.length();i++){
+            
             if(a.charAt(i)!=b.charAt(i)){
                 cnt++;
             }
+            
         }
         
         if(cnt!=1){
-            return false;    
+            return false;
         }
         return true;
+        
     }
-    
 }
