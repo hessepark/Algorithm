56d731f46fc20d9731d39536af18a350406a9a1e
hessepark <78264208+hessepark@users.noreply.github.com>
2024-12-12 23:54:04 +0900

    [level 3] Title: 단어 변환, Time: 0.36 ms, Memory: 74 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -1,59 +1,39 @@
-import java.util.*;
-import java.io.*;
-
-class Data {
-    String word;
-    int cnt;
-    public Data(String word,int cnt){
-        this.word=word;
-        this.cnt=cnt;
-    }
-}
-
-class Solution {
-        
-    public static boolean isVisited[];
+class Solution { //dfs는 비효율적인듯
+    
     public static int ans;
+    public static boolean[] isVisited;
     
     public int solution(String begin, String target, String[] words) {
-
         ans = 0;
         
-        isVisited = new boolean[words.length];
+        isVisited=new boolean[words.length];
         
-        bfs(begin,target,words);
+        dfs(begin,target,words,0);
+        
         
         return ans;
-        
     }
     
-    public static void bfs(String begin,String target,String[] words){
-        ArrayDeque<Data> q = new ArrayDeque<>();
-        q.add(new Data(begin,0));
+    public static void dfs(String cur,String target,String[] words,int cnt){
         
-        while(!q.isEmpty()){
-            Data now = q.poll();
-            
-            for(int i=0;i<words.length;i++){
-                if(!isVisited[i]&&check(now.word,words[i])){
-                    isVisited[i]=true;
-                    q.add(new Data(words[i],now.cnt+1));
-                    if(words[i].equals(target)){
-                        ans=now.cnt+1;
-                        return;
-                    }
-                }
-                
+        if(cur.equals(target)){
+            if(ans==0||cnt<ans){
+                ans=cnt;
             }
-            
-            
+        }
+        
+        for(int i=0;i<words.length;i++){
+            if(!isVisited[i]&&check(cur,words[i])){
+                isVisited[i]=true;
+                dfs(words[i],target,words,cnt+1);
+                isVisited[i]=false;
+            }
         }
         
         
-        
     }
     
-    public static boolean check(String a,String b){
+    public static boolean check(String a, String b){
         int cnt=0;
         
         for(int i=0;i<a.length();i++){
@@ -67,6 +47,7 @@ class Solution {
         if(cnt!=1){
             return false;
         }
+        
         return true;
         
     }
