5a8283c323a8543d08b08ac1f1c09cfabf5e5f01
hessepark <78264208+hessepark@users.noreply.github.com>
2024-12-15 14:31:37 +0900

    [level 3] Title: 아이템 줍기, Time: 1.10 ms, Memory: 69.2 MB -BaekjoonHub

--- 프로그래머스/3/87694. 아이템 줍기/README.md
+++ 프로그래머스/3/87694. 아이템 줍기/README.md
@@ -1,10 +1,10 @@
 # [level 3] 아이템 줍기 - 87694 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/87694#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/87694) 
 
 ### 성능 요약
 
-메모리: 73.6 MB, 시간: 1.48 ms
+메모리: 69.2 MB, 시간: 1.10 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 12월 14일 21:22:54
+2024년 12월 15일 14:31:35
 
 ### 문제 설명
 
--- 프로그래머스/3/87694. 아이템 줍기/아이템 줍기.java
+++ 프로그래머스/3/87694. 아이템 줍기/아이템 줍기.java
@@ -1,91 +1,93 @@
 import java.util.*;
 import java.io.*;
 
+//BFS
+//1. ㄷ자 같은 거 때문에 배열 두 배로 확장
+//2. 갈 수 있는 테두리 미로 만듦
+//3. 최단거리 나오면 return
+
 class Solution {
     
-    public static int dr[]={-1,0,1,0};
-    public static int dc[]={0,1,0,-1};
-    public static boolean map[][];
+    public static int dx[]={0,1,0,-1};
+    public static int dy[]={-1,0,1,0};
+    public static int visited[][];
+    public static boolean maze[][];
     public static int ans;
-    public static int isVisited[][];
     
     public int solution(int[][] rectangle, int characterX, int characterY, int itemX, int itemY) {
+        
         ans=0;
         
-        map=new boolean[102][102];
-        isVisited=new int[102][102];
+        visited=new int[102][102];
+        maze=new boolean[102][102];
         
-        //모두 true;
-        for(int data[]:rectangle){
-        for(int i=data[1]*2;i<=data[3]*2;i++){
-            for(int j=data[0]*2;j<=data[2]*2;j++){
-                map[i][j]=true;
+        for(int [] data:rectangle){
+            for(int i=data[1]*2;i<=data[3]*2;i++){
+                for(int j=data[0]*2;j<=data[2]*2;j++){
+                    maze[i][j]=true;
+                }
             }
         }
-        }
-            
-        //테두리 제외 false;
-        for(int data[]:rectangle){
-        for(int i=data[1]*2+1;i<data[3]*2;i++){
-            for(int j=data[0]*2+1;j<data[2]*2;j++){
-                map[i][j]=false; //y축x축
+        
+        //테두리만 남김
+        for(int [] data:rectangle){
+            for(int i=data[1]*2+1;i<data[3]*2;i++){
+                for(int j=data[0]*2+1;j<data[2]*2;j++){
+                    maze[i][j]=false;
+                }
             }
-        }
         }
         
         bfs(characterX,characterY,itemX,itemY);
         
-        return ans;
+        return visited[itemY*2][itemX*2]/2;
+        
+    }
+    
+    public static void bfs(int characterX, int characterY, int itemX, int itemY){
+        
+        int startX=characterX*2;
+        int startY=characterY*2;
+        int endX=itemX*2;
+        int endY=itemY*2;
+        
+        ArrayDeque<Point>q=new ArrayDeque<>();
+        q.add(new Point(startX,startY));
+        visited[startY][startX]=1; // 그림 보고 판단
+        
+        while(!q.isEmpty()){
+            
+            Point now = q.poll();
+            
+            for(int i=0;i<4;i++){
+                int nx=now.x+dx[i];
+                int ny=now.y+dy[i];
+                
+                if(nx==0||ny==0||nx==102||ny==102){
+                    continue;
+                }
+                
+                if(maze[ny][nx]&&visited[ny][nx]==0){
+                    visited[ny][nx]=visited[now.y][now.x]+1;
+                    q.add(new Point(nx,ny));
+                }
+                
+                
+            }
+            
+            
+        }
+        
         
         
     }
     
-    public static void bfs(int characterX, int characterY, int itemX,int itemY){
-        
-        int startX=characterX*2;
-        int endX=itemX*2;
-        int startY=characterY*2;
-        int endY=itemY*2;
-        
-        ArrayDeque<Point>q = new ArrayDeque<>();
-        q.add(new Point(startX,startY));
-        isVisited[startY][startX]=1;
-        
-        int cnt=0;
-        
-        while(!q.isEmpty()){
-            Point now = q.poll();
-            
-            if(now.x==endX&&now.y==endY){
-                ans= isVisited[now.y][now.x]/2;
-                return;
-            }
-            
-            for(int i=0;i<4;i++){
-                int dx=now.x+dr[i];
-                int dy=now.y+dc[i];
-                
-                if(dx==0||dy==0||dx==102||dy==102){
-                    continue;
-                }
-                
-               if (map[dy][dx]&&isVisited[dy][dx]==0) {
-                   //map[dy][dx]=false;
-                   isVisited[dy][dx]=isVisited[now.y][now.x]+1;
-                    q.add(new Point(dx, dy));
-                }
-            }
-
-        }
-        
-        
-    }
 }
 
 class Point {
     int x;
     int y;
-    public Point(int x,int y){
+    public Point(int x, int y){
         this.x=x;
         this.y=y;
     }
