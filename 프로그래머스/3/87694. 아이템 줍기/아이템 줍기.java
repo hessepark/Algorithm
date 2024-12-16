2105f1aae987ddb30b6f72dcb0eef7b55bdeb280
hessepark <78264208+hessepark@users.noreply.github.com>
2024-12-17 02:48:30 +0900

    [level 3] Title: 아이템 줍기, Time: 1.51 ms, Memory: 89.7 MB -BaekjoonHub

--- 프로그래머스/3/87694. 아이템 줍기/아이템 줍기.java
+++ 프로그래머스/3/87694. 아이템 줍기/아이템 줍기.java
@@ -2,49 +2,46 @@ import java.util.*;
 import java.io.*;
 
 //BFS
-//1. ㄷ자 같은 거 때문에 배열 두 배로 확장
-//2. 갈 수 있는 테두리 미로 만듦
-//3. 최단거리 나오면 return
+//1.맵 2배 확장
+//2.갈 수 있는 미로 맵 만들기 (상하 반전)
+//3.visited배열도 두 배
+
 
 class Solution {
     
     public static int dx[]={0,1,0,-1};
     public static int dy[]={-1,0,1,0};
+    
+    public static int visited[][];
     public static boolean maze[][];
-    public static ArrayList<Integer>result;
-    public static int cnt;
     
     public int solution(int[][] rectangle, int characterX, int characterY, int itemX, int itemY) {
-        cnt=0;
+        int answer = 0;
         
-        result=new ArrayList<>();
-
-        maze=new boolean[102][102];
+        visited = new int[102][102];
+        maze = new boolean[102][102];
         
-        for(int [] data:rectangle){
+        for(int data[]:rectangle){
             for(int i=data[1]*2;i<=data[3]*2;i++){
                 for(int j=data[0]*2;j<=data[2]*2;j++){
                     maze[i][j]=true;
                 }
             }
         }
-        
-        //테두리만 남김
-        for(int [] data:rectangle){
-            for(int i=data[1]*2+1;i<data[3]*2;i++){
+         for(int data[]:rectangle){
+              for(int i=data[1]*2+1;i<data[3]*2;i++){
                 for(int j=data[0]*2+1;j<data[2]*2;j++){
                     maze[i][j]=false;
                 }
             }
-        }
+         }
         
-        dfs(characterX,characterY,itemX,itemY);
+        bfs(rectangle, characterX, characterY,itemX,itemY);
         
-        return Math.min(result.get(0)/2,result.get(1)/2-result.get(0)/2);
-        
+        return visited[itemY*2][itemX*2]/2;
     }
     
-    public static void dfs(int characterX, int characterY, int itemX, int itemY){
+    public static void bfs(int[][] rectangle, int characterX, int characterY,int itemX,int itemY){
         
         int startX=characterX*2;
         int startY=characterY*2;
@@ -53,42 +50,29 @@ class Solution {
         
         ArrayDeque<Point>q=new ArrayDeque<>();
         q.add(new Point(startX,startY));
+        visited[startY][startX]=1;
         
-        while(true){
+        
+        while(!q.isEmpty()){
+        
+        Point now = q.poll();    
             
-            if(q.isEmpty()){ //q가 비어있을 때
-                result.add(cnt);
-                break;
+        for(int i=0;i<4;i++){
+            int nx= now.x+dx[i];
+            int ny= now.y+dy[i];
+            
+            if(nx==0||ny==0||nx==102||ny==102){
+                continue;
             }
             
-            Point now = q.pollLast();
-            
-            if(now.x==endX&&now.y==endY){ //뽑았는 게 같으면
-                result.add(cnt);
+            if(maze[ny][nx]&&visited[ny][nx]==0){
+                q.add(new Point(nx,ny));
+                visited[ny][nx]=visited[now.y][now.x]+1;
             }
-            
-            
-            for(int i=0;i<4;i++){
-                int nx=now.x+dx[i];
-                int ny=now.y+dy[i];
-                
-                if(nx==0||ny==0||nx==102||ny==102){
-                    continue;
-                }
-                
-                if(maze[ny][nx]){
-                    maze[ny][nx]=false; //if문 밑에 넣어줘도 될듯
-                    q.add(new Point(nx,ny));
-                }
-                
-            }
-            
-            cnt++;
-            
             
         }
-        
-        
+            
+        }
         
     }
     
@@ -97,7 +81,8 @@ class Solution {
 class Point {
     int x;
     int y;
-    public Point(int x, int y){
+    
+    public Point(int x,int y){
         this.x=x;
         this.y=y;
     }
