0db7ae999e9c33bbfca470c93b6c35e688cdacca
hessepark <78264208+hessepark@users.noreply.github.com>
2024-12-24 17:24:37 +0900

    [Gold IV] Title: 불, Time: 1144 ms, Memory: 109580 KB -BaekjoonHub

---
+++ 백준/Gold/5427. 불/불.java
@@ -0,0 +1,116 @@
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Scanner;
+
+class Main {
+
+	public static char maze[][];
+	public static int isVisited[][];
+	public static int isBurned[][];
+	public static int dr[] = { -1, 0, 1, 0 };
+	public static int dc[] = { 0, 1, 0, -1 };
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int t = sc.nextInt();
+
+		for (int tc = 1; tc <= t; tc++) {
+
+			int w = sc.nextInt();
+			int h = sc.nextInt();
+
+			maze = new char[h][w];
+			isVisited = new int[h][w];
+			isBurned = new int[h][w];
+
+			ArrayDeque<Point> fire = new ArrayDeque<>();
+			ArrayDeque<Point> sang = new ArrayDeque<>();
+
+			for (int i = 0; i < h; i++) {
+				String str = sc.next();
+				for (int j = 0; j < w; j++) {
+					maze[i][j] = str.charAt(j);
+					if (maze[i][j] == '@') {
+						sang.add(new Point(i, j));
+						isVisited[i][j] = 1;
+					}
+					if (maze[i][j] == '*') {
+						fire.add(new Point(i, j));
+						isBurned[i][j] = 1;
+					}
+				}
+			}
+
+			while (!fire.isEmpty()) {
+				Point now = fire.poll();
+
+				for (int i = 0; i < 4; i++) {
+
+					int nr = now.r + dr[i];
+					int nc = now.c + dc[i];
+
+					if (nr == -1 || nc == -1 || nr == h || nc == w || maze[nr][nc] == '#') {
+						continue;
+					}
+
+					if (isBurned[nr][nc] == 0) {
+						isBurned[nr][nc] = isBurned[now.r][now.c] + 1;
+						fire.add(new Point(nr, nc));
+					}
+
+				}
+			}
+
+//			for (int i = 0; i < h; i++) {
+//				System.out.println(Arrays.toString(isBurned[i]));
+//			}
+			
+			boolean isPossible=false;
+
+			while (!sang.isEmpty()) {
+				Point now = sang.poll();
+
+				if (now.r == 0 || now.c == 0 || now.r == h - 1 || now.c == w - 1) {
+					if (isVisited[now.r][now.c] < isBurned[now.r][now.c]||isBurned[now.r][now.c]==0) {
+						System.out.println(isVisited[now.r][now.c] );
+						isPossible=true;
+						break;
+					}
+				}
+
+				for (int i = 0; i < 4; i++) {
+
+					int nr = now.r + dr[i];
+					int nc = now.c + dc[i];
+
+					if (nr == -1 || nc == -1 || nr == h || nc == w || maze[nr][nc] == '#' || maze[nr][nc] == '*') {
+						continue;
+					}
+
+					if (isVisited[nr][nc] == 0) {
+						isVisited[nr][nc] = isVisited[now.r][now.c] + 1;
+						sang.add(new Point(nr, nc));
+					}
+
+				}
+			}
+			
+			if(!isPossible) {
+				System.out.println("IMPOSSIBLE");
+			}
+
+		}
+
+	}
+}
+
+class Point {
+	int r;
+	int c;
+
+	public Point(int r, int c) {
+		this.r = r;
+		this.c = c;
+	}
+}
