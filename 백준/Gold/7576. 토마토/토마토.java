9489fc2fa10275646c7c2ab88771b26efe2f1154
hessepark <78264208+hessepark@users.noreply.github.com>
2024-12-26 22:50:48 +0900

    [Gold V] Title: 토마토, Time: 1500 ms, Memory: 345896 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.java
+++ 백준/Gold/7576. 토마토/토마토.java
@@ -3,105 +3,88 @@ import java.util.Scanner;
 
 class Main {
 
-	public static int n, m;
-	public static int day;
-	public static int ans;
-	public static int currentT;
-	public static int targetT;
-	public static boolean isVisited[][];
-	public static int days[][];// 어느 시점에 들어갔는지
-	public static int box[][];
-	public static ArrayDeque<Pos> q;
-	public static int dr[] = { 1, 0, -1, 0 };
+	public static int dr[] = { -1, 0, 1, 0 };
 	public static int dc[] = { 0, 1, 0, -1 };
+
+	public static int box[][];
+	public static int isVisited[][];
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 
-		m = sc.nextInt();
-		n = sc.nextInt();
+		int m = sc.nextInt();
+		int n = sc.nextInt();
 
-		q = new ArrayDeque<>();
-		isVisited = new boolean[n][m];
 		box = new int[n][m];
-		day = 0;
-		ans = 0;
+		isVisited = new int[n][m];
+
+		ArrayDeque<Point> q = new ArrayDeque<>();
 
 		for (int i = 0; i < n; i++) {
 			for (int j = 0; j < m; j++) {
 				box[i][j] = sc.nextInt();
-				if (box[i][j] != -1) {
-					targetT++;
-				}
-				if (box[i][j] == 1) {
-					currentT++;
-					q.add(new Pos(i, j, 0));
-					isVisited[i][j] = true;
+				if (box[i][j] > 0) {
+					q.add(new Point(i, j));
+					isVisited[i][j] = 1;
 				}
 			}
 		}
 
-		if (currentT == targetT) {
-			System.out.println(day);
-			return;
-		}
-
-		else
-			bfs();
-		
-		//System.out.println("currentT"+currentT);
-		//System.out.println("targetT"+targetT);
-
-		if (currentT != targetT) {
-			day = -1;
-		}
-
-		System.out.println(day);
-
-	}
-
-	public static void bfs() {
-
+		// 익히기
 		while (!q.isEmpty()) {
-			Pos now = q.poll();
+			Point now = q.poll();
 
 			for (int i = 0; i < 4; i++) {
+
 				int nr = now.r + dr[i];
 				int nc = now.c + dc[i];
 
-				if (nr == n || nc == m || nr == -1 || nc == -1) {
+				if (nr == -1 || nc == -1 || nr == n || nc == m) {
 					continue;
 				}
 
-				if (!isVisited[nr][nc] && box[nr][nc] == 0) {
-					//System.out.println(nr + " " + nc);
-					isVisited[nr][nc] = true;
-					box[nr][nc] = 1;
-					currentT++;
-					q.add(new Pos(nr, nc, now.day + 1));
+				if (isVisited[nr][nc] == 0&&box[nr][nc]==0) {
+					isVisited[nr][nc] = isVisited[now.r][now.c] + 1;
+					q.add(new Point(nr,nc));
+					//System.out.println(nr+" "+nc);
 				}
-			}
-			//System.out.println();
 
-			if (currentT == targetT) {
-				day = now.day + 1;
-				return;
 			}
-
 		}
 
-	}
+		// 최댓값 찾기
 
+		int max = Integer.MIN_VALUE;
+		boolean isPossible = true;
+
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < m; j++) {
+				if (box[i][j] == 0 && isVisited[i][j] == 0) {
+					isPossible = false;
+					break;
+				}
+				max = Math.max(max, isVisited[i][j]);
+			}
+			if (!isPossible) {
+				break;
+			}
+		}
+		
+		if(!isPossible) {
+			System.out.println(-1);
+		}
+		
+		else System.out.println(max-1);
+
+	}
 }
 
-class Pos {
+class Point {
 	int r;
 	int c;
-	int day;
 
-	public Pos(int r, int c, int day) {
+	public Point(int r, int c) {
 		this.r = r;
 		this.c = c;
-		this.day = day;// 어느 시점에 들어갔는지
 	}
 }
