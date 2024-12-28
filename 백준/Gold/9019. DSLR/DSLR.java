31e22fe691cc304d540bedd99dd98ae0a916dfad
hessepark <78264208+hessepark@users.noreply.github.com>
2024-12-28 11:13:39 +0900

    [Gold IV] Title: DSLR, Time: 12492 ms, Memory: 321156 KB -BaekjoonHub

--- 백준/Gold/9019. DSLR/DSLR.java
+++ 백준/Gold/9019. DSLR/DSLR.java
@@ -1,9 +1,13 @@
 import java.util.ArrayDeque;
 import java.util.Scanner;
 
-class Main { // 1. subString 사용 (list 안씀)
-				// 2. visited로 안 간 숫자만 q에 넣음 (q 배열 줄어
+class Main {
 
+	// 만든 숫자 다시 안 만든다.
+	// substring으로 회전한다.
+	// 4자리 안 되면 4자리 맞춰준다.
+
+	public static boolean isVisited[];
 	public static char alpha[] = { 'D', 'S', 'L', 'R' };
 
 	public static void main(String[] args) {
@@ -16,30 +20,30 @@ class Main { // 1. subString 사용 (list 안씀)
 			String start = sc.next();
 			String target = sc.next();
 
-			// 방문 체크 배열
-			boolean[] visited = new boolean[10000];
+			isVisited = new boolean[10000];
 
 			ArrayDeque<Num> q = new ArrayDeque<>();
 			q.add(new Num(start, ""));
-			visited[Integer.parseInt(start)] = true; // 시작 숫자는 방문 처리
-
-			String ans = "";
+			isVisited[Integer.parseInt(start)] = true;
 
 			while (!q.isEmpty()) {
+
 				Num now = q.poll();
 
-				if (Integer.parseInt(target) == Integer.parseInt(now.num)) {
-					ans = now.word;
+				if (now.num.equals(target)) {
+					System.out.println(now.word);
 					break;
 				}
 
+				while (now.num.length() < 4) {
+					now.num = '0' + now.num;
+				}
+
+				// int num = Integer.parseInt(now.num);
+
 				for (int i = 0; i < 4; i++) {
 
-					while (now.num.length() < 4) { //4자리 맞춰주기
-						now.num = "0" + now.num;
-					}
-
-					int num = Integer.parseInt(now.num);
+					int num = Integer.parseInt(now.num); // 이거 여기서 해줘야함.
 
 					if (i == 0) {
 						num = num * 2;
@@ -47,34 +51,36 @@ class Main { // 1. subString 사용 (list 안씀)
 							num %= 10000;
 						}
 						// System.out.println(num);
+					}
 
-					} else if (i == 1) {
+					else if (i == 1) {
 						num = num - 1;
 						if (num == -1) {
 							num = 9999;
 						}
 						// System.out.println(num);
-
-					} else if (i == 2) {
-						String rotated = now.num.substring(1) + now.num.charAt(0);
-						num = Integer.parseInt(rotated);
-					} else if (i == 3) {
-						String rotated = now.num.charAt(now.num.length() - 1)
-								+ now.num.substring(0, now.num.length() - 1);
-						num = Integer.parseInt(rotated);
 					}
 
-					// 방문하지 않은 숫자만 큐에 추가
-					if (!visited[num]) {
-						visited[num] = true;
+					else if (i == 2) {
+						String str = now.num.substring(1) + now.num.charAt(0);
+						// String str = now.num.substring(1) + now.num.charAt(0);
+						num = Integer.parseInt(str);
+						// System.out.println("i==2 "+num);
+					}
+
+					else if (i == 3) {
+						String str = now.num.charAt(now.num.length() - 1) + now.num.substring(0, now.num.length() - 1);
+						num = Integer.parseInt(str);
+						// System.out.println("i==3 "+num);
+					}
+
+					if (!isVisited[num]) {
 						q.add(new Num(Integer.toString(num), now.word + alpha[i]));
+						isVisited[num] = true;
 					}
-
 				}
 
 			}
-
-			System.out.println(ans);
 
 		}
 
@@ -89,5 +95,4 @@ class Num {
 		this.num = num;
 		this.word = word;
 	}
-
 }
