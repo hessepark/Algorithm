e842fc3927935aaad977f752eee5a5c3bfdcded0
hessepark <78264208+hessepark@users.noreply.github.com>
2024-12-28 22:48:59 +0900

    [Gold IV] Title: DSLR, Time: 5580 ms, Memory: 325936 KB -BaekjoonHub

--- 백준/Gold/9019. DSLR/DSLR.java
+++ 백준/Gold/9019. DSLR/DSLR.java
@@ -3,10 +3,6 @@ import java.util.Scanner;
 
 class Main {
 
-	// 만든 숫자 다시 안 만든다.
-	// substring으로 회전한다.
-	// 4자리 안 되면 4자리 맞춰준다.
-
 	public static boolean isVisited[];
 	public static char alpha[] = { 'D', 'S', 'L', 'R' };
 
@@ -17,82 +13,76 @@ class Main {
 
 		for (int tc = 1; tc <= t; tc++) {
 
-			String start = sc.next();
-			String target = sc.next();
+			int start = sc.nextInt();
+			int target = sc.nextInt();
 
 			isVisited = new boolean[10000];
 
 			ArrayDeque<Num> q = new ArrayDeque<>();
-			q.add(new Num(start, ""));
-			isVisited[Integer.parseInt(start)] = true;
-
+			q.add(new Num(start, new StringBuilder()));
+			isVisited[start] = true;
+			int cnt = 0;
 			while (!q.isEmpty()) {
+				
+				cnt++;
 
 				Num now = q.poll();
+				//System.out.println(cnt+" "+now.word);
 
-				if (now.num.equals(target)) {
+				if (now.num == target) {
 					System.out.println(now.word);
 					break;
 				}
 
-				while (now.num.length() < 4) {
-					now.num = '0' + now.num;
-				}
-
-				// int num = Integer.parseInt(now.num);
+				int[] nextN = { now.calD(), now.calS(), now.calL(), now.calR() };
+				// 이렇게 안 하면 아래에서 if(i==0)이런 식으로 작업해줘야 할 거 같다.
 
 				for (int i = 0; i < 4; i++) {
-
-					int num = Integer.parseInt(now.num); // 이거 여기서 해줘야함.
-
-					if (i == 0) {
-						num = num * 2;
-						if (num > 9999) {
-							num %= 10000;
-						}
-						// System.out.println(num);
-					}
-
-					else if (i == 1) {
-						num = num - 1;
-						if (num == -1) {
-							num = 9999;
-						}
-						// System.out.println(num);
-					}
-
-					else if (i == 2) {
-						String str = now.num.substring(1) + now.num.charAt(0);
-						// String str = now.num.substring(1) + now.num.charAt(0);
-						num = Integer.parseInt(str);
-						// System.out.println("i==2 "+num);
-					}
-
-					else if (i == 3) {
-						String str = now.num.charAt(now.num.length() - 1) + now.num.substring(0, now.num.length() - 1);
-						num = Integer.parseInt(str);
-						// System.out.println("i==3 "+num);
-					}
-
-					if (!isVisited[num]) {
-						q.add(new Num(Integer.toString(num), now.word + alpha[i]));
-						isVisited[num] = true;
+					//StringBuilder sb = now.word;
+					//System.out.println(sb);
+//					if (i == 2) {
+//						System.out.println(++cnt + ": " + nextN[i]);
+//					}
+					if (!isVisited[nextN[i]]) {
+						isVisited[nextN[i]] = true;
+						q.add(new Num(nextN[i], new StringBuilder(now.word).append(alpha[i])));
 					}
 				}
 
 			}
-
 		}
 
 	}
 }
 
 class Num {
-	String num;
-	String word;
+	int num;
+	StringBuilder word;
 
-	public Num(String num, String word) {
+	public Num(int num, StringBuilder word) {
 		this.num = num;
 		this.word = word;
+	}
+
+	int calD() {
+		return (num * 2) % 10000;
+	}
+
+	int calS() {
+		// num = num - 1; 여기서 값 바꾸면 안 됨
+//		if (num == -1) {
+//			num = 9999;
+//		}
+		return num - 1 == -1 ? 9999 : num - 1;
+	}
+
+	// 1234->2341 //0123->1230//
+	int calL() {
+		return (num % 1000) * 10 + num / 1000;
+	}
+
+	// 1234->4123 // 0123->3012
+	int calR() {
+		return (num % 10) * 1000 + num / 10;
 	}
 }
