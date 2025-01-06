52ef7e8150ad3f2b8f7b9e3705cc7bdb2d6b0ec5
hessepark <78264208+hessepark@users.noreply.github.com>
2025-01-06 14:04:30 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 9.87 ms, Memory: 55 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,54 +1,54 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 class Solution {
     
-    public static int dr[]={-1,0,1,0};
-    public static int dc[]={0,1,0,-1};
-    public static int visited[][];
+    public static int dr[] = {-1,0,1,0};
+    public static int dc[] = {0,1,0,-1};
+    public static int isVisited[][];
     
-    public int solution(int[][] maps) { //maps 배열 인덱스는 0베이스라는 것에 주의하자.
-    
-        visited=new int[maps.length+1][maps[0].length+1];
+    public int solution(int[][] maps) { //map 0 based
+        
+        isVisited=new int[maps.length+1][maps[0].length+1]; //1 based
         
         bfs(maps);
         
-        if(visited[maps.length][maps[0].length]==0){
-            return -1;
+        if(isVisited[maps.length][maps[0].length]==0){
+            isVisited[maps.length][maps[0].length]=-1;
         }
         
-        return visited[maps.length][maps[0].length];
-
+        return isVisited[maps.length][maps[0].length];
     }
     
     public static void bfs(int[][] maps){
-        
+
         ArrayDeque<Point>q = new ArrayDeque<>();
         q.add(new Point(1,1));
-        visited[1][1]=1;
+        isVisited[1][1]=1;
         
         while(!q.isEmpty()){
+            
             Point now = q.poll();
             
             for(int i=0;i<4;i++){
-                int nr=now.r+dr[i];
-                int nc=now.c+dc[i];
                 
-                if(nc==0||nr==0||nc==maps[0].length+1||nr==maps.length+1){
+                int nr = now.r+dr[i];
+                int nc = now.c+dc[i];
+                
+                if(nr==0||nc==0||nr==maps.length+1||nc==maps[0].length+1){
                     continue;
                 }
                 
-                if(visited[nr][nc]==0&&maps[nr-1][nc-1]==1){
+                if(maps[nr-1][nc-1]==1&&isVisited[nr][nc]==0){
                     q.add(new Point(nr,nc));
-                    visited[nr][nc]=visited[now.r][now.c]+1;
+                    isVisited[nr][nc]=isVisited[now.r][now.c]+1;
                 }
-                
                 
             }
             
             
+            
         }
-        
         
     }
 }
@@ -56,6 +56,7 @@ class Solution {
 class Point {
     int r;
     int c;
+    
     public Point(int r,int c){
         this.r=r;
         this.c=c;
