4c8111974357a92da95995d9532139927028c98b
hessepark <78264208+hessepark@users.noreply.github.com>
2025-01-13 10:17:17 +0900

    [level 1] Title: 모의고사, Time: 1.33 ms, Memory: 75.4 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -1,49 +1,40 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 class Solution {
     public int[] solution(int[] answers) {
         int[] answer = {};
         
-        int p[][] = { {1,2,3,4,5},{2,1,2,3,2,4,2,5},{3,3,1,1,2,2,4,4,5,5}};
+        int solve [][] = { {1,2,3,4,5}, {2,1,2,3,2,4,2,5}, {3,3,1,1,2,2,4,4,5,5}};
+        int cnt [] = new int[3];
         
-        int cnt[]=new int[3];
-        
-        int max=Integer.MIN_VALUE;
-       
-        for(int i=0;i<3;i++){
-            int num=0;
-            for(int j=0;j<answers.length;j++){
-                if(answers[j]==p[i][j%p[i].length]){
-                    num++;
+        for(int i=0;i<solve.length;i++){
+                for(int j=0;j<answers.length;j++){
+                    if(answers[j]==solve[i][j%solve[i].length]){
+                        cnt[i]++;
+                    }
                 }
-            }
-            cnt[i]=num;
-            max=Math.max(max,cnt[i]);
         }
         
-        ArrayList<Integer>list = new ArrayList<>();
+        int max = Integer.MIN_VALUE;
+        
+        for(int i=0;i<cnt.length;i++){
+            max = Math.max(max,cnt[i]);
+        }
+        
+        ArrayList<Integer>result = new ArrayList<>();
         
         for(int i=0;i<cnt.length;i++){
             if(cnt[i]==max){
-                list.add(i+1);
+                result.add(i+1);
             }
         }
         
-        answer=new int[list.size()];
+        answer = new int[result.size()];
         
         for(int i=0;i<answer.length;i++){
-            answer[i]=list.get(i);
+            answer[i] = result.get(i);
         }
-        
-        
-        
-
-        
-        
-    //System.out.println(Arrays.toString(cnt));
-        
-        
         
         return answer;
     }
