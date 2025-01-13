b068b7b5ef6648cf0dbcfb218784e44ba5f6ba96
hessepark <78264208+hessepark@users.noreply.github.com>
2025-01-13 16:23:19 +0900

    [level 2] Title: 카펫, Time: 14.44 ms, Memory: 74.3 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -1,26 +1,21 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 class Solution {
     public int[] solution(int brown, int yellow) {
         int[] answer = new int[2];
         
-        int result=brown+yellow;
+        int sum = brown + yellow;
         
-        int a=-1;
-        int b=-1;
-        
-        for(int i=3;i<=Math.sqrt(result);i++){ //최소 3이상이어야 노란색이 들어갈 공간이 남
-            if(result%i==0){
-                if((i-2)*(result/i-2)==yellow){ //이 조건 써주지 않으면 테스트케이스 4,6,7 에러남
-                    a=result/i;
-                    b=i;
-                }
+        for(int i=3;i<sum;i++){
+            if(sum%i==0){
+                //System.out.println(i);
+               if((i-2)*((sum/i)-2)==yellow){
+                   answer[0]=i;
+                   answer[1]=sum/i;
+               }
             }
         }
-        
-        answer[0]=a;
-        answer[1]=b;
         
         return answer;
     }
