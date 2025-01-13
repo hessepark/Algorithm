7b53dfb4c92ac7afa1ec2911efc4d307466990f8
hessepark <78264208+hessepark@users.noreply.github.com>
2025-01-13 10:00:30 +0900

    [level 1] Title: 최소직사각형, Time: 2.13 ms, Memory: 82.5 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
@@ -1,29 +1,21 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 class Solution {
     public int solution(int[][] sizes) {
         int answer = 0;
         
-        //젤 큰 거랑 
-        //작은 거 중에 젤 큰 걸 찾으면 된다.
-        int max=Integer.MIN_VALUE; //젤 큰 거
-        int minMax=Integer.MIN_VALUE; //작은 거 중에 젤 큰 거 
+        int max = Integer.MIN_VALUE; //젤 큰 거
+        int minMax = Integer.MIN_VALUE; //작은 거 중에 젤 큰 거
         
         for(int i=0;i<sizes.length;i++){
-            int min=Integer.MAX_VALUE;
-            for(int j=0;j<2;j++){
-                max=Math.max(max,sizes[i][j]);
-                min=Math.min(min,sizes[i][j]);
+            for(int j=1;j<sizes[0].length;j++){
+               max=Math.max(max,Math.max(sizes[i][0],sizes[i][1]));
+               minMax=Math.max(minMax,Math.min(sizes[i][0],sizes[i][1]));
             }
-            minMax=Math.max(minMax,min);
         }
         
-        System.out.println(max);
-        System.out.println(minMax);
-        
-        answer=max*minMax;
-        
+        answer = max * minMax;
         
         return answer;
     }
