b228d1f554fb9915e902fc322ab27c41d52a35eb
hessepark <78264208+hessepark@users.noreply.github.com>
2025-01-14 09:44:41 +0900

    [level 2] Title: 피로도, Time: 3.00 ms, Memory: 78.9 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -1,36 +1,35 @@
+import java.util.*;
+import java.io.*;
+
 class Solution {
     
     public static boolean isVisited[];
     public static int max;
     
     public int solution(int k, int[][] dungeons) {
-      
-        max = Integer.MIN_VALUE;
-        isVisited=new boolean[dungeons.length+1];
         
-        perm(k,0,dungeons);//피로도,깊이
+        isVisited = new boolean[dungeons.length];
+        max=Integer.MIN_VALUE;
+        
+        dfs(k,0,dungeons); //현재 피로도, 깊이, 던전
         
         return max;
     }
     
-    public static void perm(int cur,int depth,int[][] dungeons) {
-        
-        max=Math.max(max,depth);
-        
-        if(depth==dungeons.length){
-            return;
-        }
+    public static void dfs(int cur,int depth,int[][]dungeons){
+    
+        max=Math.max(depth,max);
         
         for(int i=0;i<dungeons.length;i++){
-            if(cur>=dungeons[i][0]&&!isVisited[i]){
+            if(!isVisited[i]&&cur>=dungeons[i][0]){
                 isVisited[i]=true;
-               //System.out.println(i);
-                perm(cur-dungeons[i][1],depth+1,dungeons);
+                dfs(cur-dungeons[i][1],depth+1,dungeons);
                 isVisited[i]=false;
             }
         }
         
         
     }
+    
     
 }
