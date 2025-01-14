51625df7a7f9b57bbaa685920478feee19ba55e2
hessepark <78264208+hessepark@users.noreply.github.com>
2025-01-14 09:29:33 +0900

    [level 2] Title: 소수 찾기, Time: 3.05 ms, Memory: 75.5 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -4,53 +4,54 @@ import java.io.*;
 class Solution {
     
     public static HashSet<Integer>set;
+    public static boolean[] isVisited;
     public static int ans;
-    public static boolean isVisited[];
     
     public int solution(String numbers) {
         ans = 0;
-        
         set = new HashSet<>();
         isVisited = new boolean[numbers.length()];
         
-        perm(0,0,numbers); //현재 값, 깊이
+        perm(0,0,numbers);//현재 숫자,깊이,String
         
         return ans;
     }
     
-    public static void perm(int cur,int depth,String numbers){
+    public static void perm(int num, int depth, String numbers){
         
-        if(isPrime(cur)&&cur>=2){
-            set.add(cur);
+        if(isPrime(num)){
+            set.add(num);
             ans++;
         }
         
-        if(depth==numbers.length()){ //이게 없으면 012 다 차고도 또 012 한 번 더 봐야함
+        if(depth==numbers.length()){
             return;
         }
         
         for(int i=0;i<numbers.length();i++){
             if(!isVisited[i]){
                 isVisited[i]=true;
-                perm(cur*10+numbers.charAt(i)-'0',depth+1,numbers);
+                perm(num*10+numbers.charAt(i)-'0',depth+1,numbers);
                 isVisited[i]=false;
             }
         }
+        
         
     }
     
     public static boolean isPrime(int num){
         
-        if(set.contains(num)){
+        if(set.contains(num)||num<2){
             return false;
         }
         
-        for(int i=2;i<=(int)Math.sqrt(num);i++){
+        for(int i=2;i<=Math.sqrt(num);i++){
             if(num%i==0){
                 return false;
             }
         }
+        
         return true;
+        
     }
-
 }
