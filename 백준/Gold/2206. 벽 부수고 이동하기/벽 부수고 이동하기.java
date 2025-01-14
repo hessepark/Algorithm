7c2b97ef0a86e2d6695b940c7d705af822a1f584
hessepark <78264208+hessepark@users.noreply.github.com>
2025-01-14 15:40:34 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 764 ms, Memory: 98424 KB -BaekjoonHub

--- 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.java
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.java
@@ -1,98 +1,79 @@
 import java.util.ArrayDeque;
 import java.util.Scanner;
 
-public class Main {
+class Main {
 
-	// 상하좌우 보면서 바로 갈 수 있는 거 넣고,
-	// 벽 하나 부수고 갈 수 있는 거 넣는다.
+	public static int n, m;
 	public static int maze[][];
 	public static int isVisited[][][];
-	public static int isCrush[][];
 	public static int dr[] = { -1, 0, 1, 0 };
 	public static int dc[] = { 0, 1, 0, -1 };
 
 	public static void main(String[] args) {
+
 		Scanner sc = new Scanner(System.in);
 
-		int n = sc.nextInt();
-		int m = sc.nextInt();
+		n = sc.nextInt();
+		m = sc.nextInt();
 
-		maze = new int[n][m];
-		isVisited = new int[n][m][2];
+		maze = new int[n + 1][m + 1];
+		isVisited = new int[n + 1][m + 1][2];
 
-		for (int i = 0; i < n; i++) {
+		for (int i = 1; i < n + 1; i++) {
 			String str = sc.next();
-			for (int j = 0; j < m; j++) {
-				maze[i][j] = str.charAt(j) - '0';
+			for (int j = 1; j < m + 1; j++) {
+				maze[i][j] = str.charAt(j-1)-'0';
 			}
 		}
-
-		// 벽 안 부수고 갈 수 있는 최단거리
 
 		ArrayDeque<Point> q = new ArrayDeque<>();
-		isVisited[0][0][0] = 1;
-		q.add(new Point(0, 0, 0));
+		q.add(new Point(1, 1, 0));
+		isVisited[1][1][0]=1;
 
 		while (!q.isEmpty()) {
-			Point now = q.poll();
 
-			if (now.r == n - 1 && now.c == m - 1) {
-				
-//				for(int i=0;i<n;i++) {
-//					for(int j=0;j<m;j++) {
-//						System.out.printf("%02d ", isVisited[i][j][0]);
-//					}
-//					System.out.println();
-//				}
-//				
-//				System.out.println();
-//				
-//				for(int i=0;i<n;i++) {
-//					for(int j=0;j<m;j++) {
-//						System.out.printf("%02d ", isVisited[i][j][1]);
-//					}
-//					System.out.println();
-//				}
-				
-				
-				System.out.println(isVisited[now.r][now.c][now.crush]);
+			Point now = q.poll();
+			
+			if(now.r==n&&now.c==m) {
+				System.out.println(isVisited[now.r][now.c][now.isBroken]);
 				return;
 			}
-
-			for (int i = 0; i < 4; i++) {
-				int nr = dr[i] + now.r;
-				int nc = dc[i] + now.c;
-
-				if (nr == -1 || nc == -1 || nr == n || nc == m) {
+			
+			for(int i=0;i<4;i++) {
+				
+				int nr= dr[i]+now.r;
+				int nc= dc[i]+now.c;
+				
+				if(nr==0||nc==0||nr==n+1||nc==m+1) {
 					continue;
 				}
-
-				if (isVisited[nr][nc][now.crush] == 0) {
-					if (maze[nr][nc] == 0) { //그 위치까지 부수고 간 것인지, 아닌지는 알고 있어야함
-						isVisited[nr][nc][now.crush] = isVisited[now.r][now.c][now.crush] + 1;
-						q.add(new Point(nr, nc, now.crush));
-					} else if (maze[nr][nc] == 1 && now.crush == 0&&isVisited[nr][nc][1]==0) { 
-						q.add(new Point(nr, nc, 1));
-						isVisited[nr][nc][1]=isVisited[now.r][now.c][now.crush]+1;
-					}
+				
+				if(isVisited[nr][nc][now.isBroken]==0&&maze[nr][nc]==0) {
+					isVisited[nr][nc][now.isBroken]=isVisited[now.r][now.c][now.isBroken]+1;
+					q.add(new Point(nr,nc,now.isBroken));
 				}
-
+				else if(now.isBroken==0&&isVisited[nr][nc][1]==0) {
+					isVisited[nr][nc][1]=isVisited[now.r][now.c][now.isBroken]+1;
+					q.add(new Point(nr,nc,1));
+				}
+				
 			}
+
 		}
 		
-		System.out.println(-1);
+		System.out.println("-1");
+
 	}
-	
 }
 
 class Point {
 	int r;
 	int c;
-	int crush;
+	int isBroken;
 
-	public Point(int r, int c, int crush) {
+	public Point(int r, int c, int isBroken) {
 		this.r = r;
 		this.c = c;
-		this.crush = crush;
+		this.isBroken = isBroken;
 	}
 }
