ae0b93e8502f7c8b7037e162f43e8f2e91af430a
hessepark <78264208+hessepark@users.noreply.github.com>
2025-01-17 03:35:30 +0900

    [Bronze I] Title: 피보나치 수 2, Time: 172 ms, Memory: 17712 KB -BaekjoonHub

--- 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.java
+++ 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.java
@@ -2,26 +2,30 @@ import java.util.Scanner;
 
 class Main {
 
-	public static long mem[] = new long[95];
+	public static long cache[];
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+
 		int n = sc.nextInt();
+		
+		cache = new long[n + 1];
 
 		System.out.println(fibo(n));
 	}
 
-	public static long fibo(int n) {
+	public static long fibo(int num) {
 
-		mem[1] = 1;
-
-		for (int i = 1; i <= n; i++) {
-			mem[i + 1] += mem[i];
-			mem[i + 2] += mem[i];
+		if(cache[num]!=0) {
+			return cache[num];
+		}
+		
+		
+		if (num == 1 || num == 2) {
+			return 1;
 		}
 
-		return mem[n];
-
+		cache[num] = fibo(num-1) + fibo(num-2);
+		return cache[num];
 	}
-
 }
