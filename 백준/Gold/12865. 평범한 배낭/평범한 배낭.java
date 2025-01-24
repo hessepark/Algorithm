25b413f5bbfdb971c251b91d6343aa7dca4a9dc5
hessepark <78264208+hessepark@users.noreply.github.com>
2025-01-24 15:49:44 +0900

    [Gold V] Title: 평범한 배낭, Time: 212 ms, Memory: 18452 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -1,16 +1,13 @@
 import java.util.Scanner;
 
 class Main {
-	
-	public static int dp[];
-	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
 		int n = sc.nextInt();
 		int k = sc.nextInt();
 		
-		dp = new int[k+1];
+		int dp[]=new int[k+1];
 		
 		for(int i=0;i<n;i++) {
 			int w = sc.nextInt();
@@ -19,6 +16,7 @@ class Main {
 			for(int j=k;j-w>=0;j--) {
 				dp[j]=Math.max(dp[j], dp[j-w]+v);
 			}
+			
 		}
 		
 		System.out.println(dp[k]);
