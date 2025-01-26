82ce561b1a50af41ee7063199c37493b82793316
hessepark <78264208+hessepark@users.noreply.github.com>
2025-01-26 17:26:48 +0900

    [Silver I] Title: 동물원, Time: 180 ms, Memory: 18836 KB -BaekjoonHub

--- 백준/Silver/1309. 동물원/동물원.java
+++ 백준/Silver/1309. 동물원/동물원.java
@@ -1,35 +1,28 @@
 import java.util.Scanner;
 
 class Main {
-
-	public static int L = 0;
-	public static int R = 1;
-	public static int N = 2;
-
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 
 		int n = sc.nextInt();
 
-		int d[][] = new int[3][n + 1];
+		final int NONE = 0;
+		final int LEFT = 1;
+		final int RIGHT = 2;
 
-		d[L][1] = 1;
-		d[R][1] = 1;
-		d[N][1] = 1;
+		int dp[][] = new int[3][n + 1];
 
-		for (int i = 2; i < n + 1; i++) {
-			d[L][i] = (d[R][i - 1] + d[N][i - 1]) % 9901;
-			d[R][i] = (d[L][i - 1] + d[N][i - 1]) % 9901;
-			d[N][i] = (d[L][i - 1] + d[R][i - 1] + d[N][i - 1]) % 9901;
+		dp[NONE][1] = 1;
+		dp[LEFT][1] = 1;
+		dp[RIGHT][1] = 1;
+
+		for (int i = 2; i <= n; i++) {
+			dp[NONE][i] = (dp[NONE][i - 1] + dp[LEFT][i - 1] + dp[RIGHT][i - 1])%9901;
+			dp[LEFT][i] = (dp[NONE][i - 1] + dp[RIGHT][i - 1])%9901;
+			dp[RIGHT][i] = (dp[NONE][i - 1] + dp[LEFT][i - 1])%9901;
 		}
-
-		int sum = 0;
-
-		for (int i = 0; i < 3; i++) {
-			sum = (sum + d[i][n]) % 9901;
-		}
-
-		System.out.println(sum);
+		
+		System.out.println((dp[NONE][n]+dp[LEFT][n]+dp[RIGHT][n])%9901);
 
 	}
 }
