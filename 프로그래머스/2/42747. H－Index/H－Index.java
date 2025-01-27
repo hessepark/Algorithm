736495728d892f3ffb8cb31b18760233d5748537
hessepark <78264208+hessepark@users.noreply.github.com>
2025-01-28 07:46:12 +0900

    [level 2] Title: H-Index, Time: 2.54 ms, Memory: 73.5 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.java
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -1,24 +1,6 @@
-import java.util.*;
-import java.io.*;
-
 class Solution {
     public int solution(int[] citations) {
         int answer = 0;
-        
-        ArrayList<Integer>list = new ArrayList<>();
-        
-        for(int i=0;i<citations.length;i++){
-            list.add(citations[i]);
-        }
-        
-        Collections.sort(list,Collections.reverseOrder());
-        
-        for(int i=0;i<list.size();i++){
-            if(list.get(i)<i+1){
-                return i;
-            }
-        }
-        
-        return list.size();
+        return answer;
     }
 }
