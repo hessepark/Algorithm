7babe4631a4f1513111dcc3ffc0243d4eae16e24
hessepark <78264208+hessepark@users.noreply.github.com>
2025-01-28 06:20:08 +0900

    [level 1] Title: K번째수, Time: 0.47 ms, Memory: 74.2 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -5,25 +5,24 @@ class Solution {
     public int[] solution(int[] array, int[][] commands) {
         int[] answer = new int[commands.length];
         
-        int idx=0;
-        
         //자르기, 정렬하기, 꺼내기
         
         for(int i=0;i<commands.length;i++){
-            int start=commands[i][0]-1;
-            int end=commands[i][1]-1;
-            int pick=commands[i][2]-1;
+            int start=commands[i][0];
+            int end=commands[i][1];
+            int pick=commands[i][2];
             
             int[] arr = new int[end-start+1];
             
-            for(int j=0;j<arr.length;j++){
-               arr[j]=array[start++];
-            }
+            int idx=0;
             
+            for(int j=start-1;j<=end-1;j++){
+               arr[idx++]=array[j];
+            }
             
             Arrays.sort(arr);
             
-            answer[idx++]=arr[pick];
+            answer[i]=arr[pick-1];
             
         }
         
