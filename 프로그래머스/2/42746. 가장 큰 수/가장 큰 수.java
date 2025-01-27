7445cdf05ab0deacc4d63f7026e75a9d7b18caae
hessepark <78264208+hessepark@users.noreply.github.com>
2025-01-28 06:46:51 +0900

    [level 2] Title: 가장 큰 수, Time: 214.20 ms, Memory: 133 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -1,28 +1,27 @@
-import java.util.ArrayList;
-import java.util.Collections;
+import java.util.*;
+import java.io.*;
 
 class Solution {
-	public String solution(int[] numbers) {
-		ArrayList<String> answer = new ArrayList<>();
-
-		for (int i = 0; i < numbers.length; i++) {
-			answer.add(numbers[i]+"");
-		}
-
-		Collections.sort(answer, (o1, o2) -> (o2+o1).compareTo(o1+o2));
-
-		//System.out.println(answer);
-
-		if (answer.get(0).equals("0")) {
-			return "0";
-		}
-
-		StringBuilder sb = new StringBuilder();
-
-		for (int i = 0; i < answer.size(); i++) {
-			sb.append(answer.get(i));
-		}
-
-		return sb.toString();
-	}
+    public String solution(int[] numbers) {
+        String[] answer = new String[numbers.length];
+        
+        //문자를 붙여서 비교해야함
+        for(int i=0;i<numbers.length;i++){
+            answer[i]=numbers[i]+"";
+        }
+        
+        Arrays.sort(answer,(o1,o2)-> (o2+o1).compareTo(o1+o2));
+        
+        if(answer[0].equals("0")){
+            return "0";
+        }
+        
+        StringBuilder sb = new StringBuilder();
+        
+        for(int i=0;i<answer.length;i++){
+            sb.append(answer[i]);
+        }
+        
+        return sb.toString();
+    }
 }
