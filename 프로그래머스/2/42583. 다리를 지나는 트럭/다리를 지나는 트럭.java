1a01e66e609f5e795d7928f604ecc2d689944fe7
hessepark <78264208+hessepark@users.noreply.github.com>
2025-01-29 00:20:45 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 23.56 ms, Memory: 89.1 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
@@ -1,44 +1,44 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 class Solution {
     public int solution(int bridge_length, int weight, int[] truck_weights) {
         int answer = 0;
         
-        ArrayDeque<Integer>q = new ArrayDeque<>(); //다리 위 트럭들 큐
-        int present_sum=0; //현재 다리 위 트럭들 무게
-        int time=0; //걸리는 시간 변수
+        ArrayDeque<Integer>q = new ArrayDeque<>();
+        int total_weight=0;
+        int time=0;
         
-        for(int i=0;i<truck_weights.length;i++) {
-            int truck=truck_weights[i];
+        for(int i=0; i<truck_weights.length;i++){
             
             while(true){
+                
+            if(q.size()<bridge_length){
+                if(total_weight+truck_weights[i]<=weight){
+                    q.add(truck_weights[i]);
+                    total_weight+=truck_weights[i];
+                    time++;
+                    break;
+                }
+                
+                else{
+                    q.add(0);
+                    time++;
+                }
+             }
+                
+                if(q.size()==bridge_length){
+                    total_weight-=q.peekFirst();
+                    q.poll();
+                }
+                
+                
+            }
             
-            if(q.size()==bridge_length){
-                present_sum-=q.poll();
-                //System.out.println(q);
-            }
-                time++;
-            if(present_sum+truck<=weight){
-                q.add(truck);
-                present_sum+=truck;
-                //System.out.println(q);
-                break;
-            }
-            else{
-                q.add(0);
-                //System.out.println(q);
-            }
-            
-            }
         }
-        
-        //System.out.println(q);
         
         time+=bridge_length;
         
-        answer=time;
-        
-        return answer;
+        return time;
     }
 }
