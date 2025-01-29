4cca1396ff9a8cacf70502297770238ca8f5a513
hessepark <78264208+hessepark@users.noreply.github.com>
2025-01-30 02:21:46 +0900

    [level 2] Title: 모음 사전, Time: 17.88 ms, Memory: 81.4 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.java
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.java
@@ -1,44 +1,43 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 class Solution {
     
-    public static char alpha[] = {'A','E','I','O','U'};
     public static int ans;
-    public static boolean isFind;
+    public static int num;
+    public static char[] alpha= {'A','E','I','O','U'};
     
     public int solution(String word) {
-        ans = 0;
-        isFind=false;
+        ans=0;
         
         dfs("",word);
         
         return ans;
     }
     
-    public static void dfs(String now,String word) {
+    public static void dfs (String cur,String word){
         
-        if(now.equals(word)){
-            isFind=true;
-           // System.out.println("정답: "+now+" 도달: "+ans);
+       // System.out.println(cur);
+        
+        
+        if(cur.equals(word)){
+            System.out.println(num);
+            ans=num;
             return;
         }
         
-        if(now.length()==5){
-           // System.out.println("도달 "+now);
+        if(cur.length()==5){
             return;
         }
         
-        for(int i=0;i<alpha.length;i++){
-            ans++;
-           // System.out.println(now);
-            dfs(now+alpha[i],word);
-            if(isFind){
+        for(int i=0;i<5;i++){
+            num++;
+            dfs(cur+alpha[i],word);
+            if(ans!=0){
                 return;
             }
-             //System.out.println("리턴 "+now +" i: "+i);
         }
         
-        
     }
+    
 }
