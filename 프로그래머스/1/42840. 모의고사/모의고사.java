05bf6900f6a0178c8dc6a87e0c8160e578626f3d
hessepark <78264208+hessepark@users.noreply.github.com>
2025-01-29 21:12:49 +0900

    [level 1] Title: 모의고사, Time: 3.60 ms, Memory: 85.6 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -3,20 +3,20 @@ import java.io.*;
 
 class Solution {
     public int[] solution(int[] answers) {
-        int[] answer;
+        int[] answer = {};
         
-        int solve [][] = { {1,2,3,4,5}, {2,1,2,3,2,4,2,5}, {3,3,1,1,2,2,4,4,5,5}};
-        int cnt [] = new int[3];
+        int[][] list = { {1,2,3,4,5}, {2,1,2,3,2,4,2,5}, {3,3,1,1,2,2,4,4,5,5}};
+        int[] cnt = new int[list.length];
         
-        int max = Integer.MIN_VALUE;
+        int max=Integer.MIN_VALUE;
         
-        for(int i=0;i<solve.length;i++){
-                for(int j=0;j<answers.length;j++){
-                    if(answers[j]==solve[i][j%solve[i].length]){
-                        cnt[i]++;
-                        max = Math.max(cnt[i],max);
-                    }
+        for(int i=0;i<list.length;i++){       
+            for(int j=0;j<answers.length;j++){
+                if(answers[j]==list[i][j%list[i].length]) {
+                    cnt[i]++;
                 }
+                max=Math.max(max,cnt[i]);
+            }
         }
         
         ArrayList<Integer>result = new ArrayList<>();
@@ -30,7 +30,7 @@ class Solution {
         answer = new int[result.size()];
         
         for(int i=0;i<answer.length;i++){
-            answer[i] = result.get(i);
+            answer[i]=result.get(i);
         }
         
         return answer;
