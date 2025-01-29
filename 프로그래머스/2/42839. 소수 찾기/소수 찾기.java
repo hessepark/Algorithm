49979952d94775fc088b3b0aa72e5da2013c645e
hessepark <78264208+hessepark@users.noreply.github.com>
2025-01-29 23:23:31 +0900

    [level 2] Title: 소수 찾기, Time: 21.46 ms, Memory: 95.5 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -1,57 +1,58 @@
-import java.util.*;
-import java.io.*;
+import java.util.HashSet;
 
 class Solution {
-    
-    public static HashSet<Integer>set;
-    public static boolean[] isVisited;
-    public static int ans;
-    
-    public int solution(String numbers) {
-        ans = 0;
-        set = new HashSet<>();
-        isVisited = new boolean[numbers.length()];
-        
-        perm(0,0,numbers);//현재 숫자,깊이,String
-        
-        return ans;
-    }
-    
-    public static void perm(int num, int depth, String numbers){
-        
-        if(isPrime(num)){
-            set.add(num);
-            ans++;
-        }
-        
-        if(depth==numbers.length()){
-            return;
-        }
-        
-        for(int i=0;i<numbers.length();i++){
-            if(!isVisited[i]){
-                isVisited[i]=true;
-                perm(num*10+numbers.charAt(i)-'0',depth+1,numbers);
-                isVisited[i]=false;
-            }
-        }
-        
-        
-    }
-    
-    public static boolean isPrime(int num){
-        
-        if(set.contains(num)||num<2){
-            return false;
-        }
-        
-        for(int i=2;i<=Math.sqrt(num);i++){
-            if(num%i==0){
-                return false;
-            }
-        }
-        
-        return true;
-        
-    }
+
+	// 시작점에서부터 다 본다.
+
+	public static HashSet<Integer> set = new HashSet<>();
+	public static boolean isVisited[];
+	public static int ans;
+
+	public int solution(String numbers) {
+		ans = 0;
+
+		isVisited = new boolean[numbers.length()];
+
+		dfs("", numbers);
+
+		return ans;
+	}
+
+	public static void dfs(String cur, String numbers) {
+
+		//System.out.println(cur);
+
+		if (cur.length()!=0&&isPrime(Integer.parseInt(cur))) {
+			set.add(Integer.parseInt(cur));
+			ans++;
+		}
+
+		for (int i = 0; i < numbers.length(); i++) {
+			if (!isVisited[i]) {
+				isVisited[i] = true;
+
+				dfs(cur + numbers.charAt(i), numbers);
+				//System.out.println("종료: " + numbers.charAt(i) + "종료 당시 cur:" + cur);
+				isVisited[i] = false;
+			}
+			// isVisited[i]=false; 여기 들어가면 121 나온다. 이유는 1이 안 간 거 처리 돼버림
+		}
+
+	}
+
+	public static boolean isPrime(int num) {
+
+		if (num<=1||set.contains(num)) {
+			return false;
+		}
+
+		for (int i = 2; i <= Math.sqrt(num); i++) {
+			if (num % i == 0) {
+				return false;
+			}
+		}
+
+		return true;
+
+	}
 }
