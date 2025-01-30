5974d65b506e4ca0f17892338334d99f69f22f65
hessepark <78264208+hessepark@users.noreply.github.com>
2025-01-30 19:13:43 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 7.72 ms, Memory: 55.1 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -5,59 +5,54 @@ class Solution {
     
     public static int dr[] = {-1,0,1,0};
     public static int dc[] = {0,1,0,-1};
-    public static int isVisited[][];
+    public static int visited[][];
+   
     
-    public int solution(int[][] maps) { //map 0 based
+    public int solution(int[][] maps) {
         
-        isVisited=new int[maps.length+1][maps[0].length+1]; //1 based
+        visited=new int[maps.length+1][maps[0].length+1];
         
         bfs(maps);
         
-        if(isVisited[maps.length][maps[0].length]==0){
-            isVisited[maps.length][maps[0].length]=-1;
+        if(visited[maps.length][maps[0].length]==0){
+            return -1;
         }
         
-        return isVisited[maps.length][maps[0].length];
+        return visited[maps.length][maps[0].length];
     }
     
     public static void bfs(int[][] maps){
-
-        ArrayDeque<Point>q = new ArrayDeque<>();
-        q.add(new Point(1,1));
-        isVisited[1][1]=1;
-        
-        while(!q.isEmpty()){
+    
+    ArrayDeque<Point> q = new ArrayDeque<>();
+    visited[1][1]=1;
+    q.add(new Point(1,1));
+    
+    while(!q.isEmpty()){
+        Point now = q.poll();
+        for(int i=0;i<4;i++){
+            int nr= dr[i]+now.r;
+            int nc= dc[i]+now.c;
             
-            Point now = q.poll();
-            
-            for(int i=0;i<4;i++){
-                
-                int nr = now.r+dr[i];
-                int nc = now.c+dc[i];
-                
-                if(nr==0||nc==0||nr==maps.length+1||nc==maps[0].length+1){
-                    continue;
-                }
-                
-                if(maps[nr-1][nc-1]==1&&isVisited[nr][nc]==0){
-                    q.add(new Point(nr,nc));
-                    isVisited[nr][nc]=isVisited[now.r][now.c]+1;
-                }
-                
+            if(nr==0||nc==0||nr==maps.length+1||nc==maps[0].length+1||maps[nr-1][nc-1]==0){
+                continue;
             }
             
-            
-            
+            if(visited[nr][nc]==0){
+               visited[nr][nc]=visited[now.r][now.c]+1;
+               q.add(new Point(nr,nc));
+            }
         }
-        
+    }
+    
     }
 }
+
+
 
 class Point {
     int r;
     int c;
-    
-    public Point(int r,int c){
+    public Point(int r, int c){
         this.r=r;
         this.c=c;
     }
