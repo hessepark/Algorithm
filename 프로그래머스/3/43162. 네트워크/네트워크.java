4dff8c8fc76753007e7a1c5ea4a0884d56ab4897
hessepark <78264208+hessepark@users.noreply.github.com>
2025-01-30 18:33:07 +0900

    [level 3] Title: 네트워크, Time: 0.18 ms, Memory: 85.6 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -3,32 +3,32 @@ import java.io.*;
 
 class Solution {
     
+    public static int cnt;
     public static boolean isVisited[];
     
     public int solution(int n, int[][] computers) {
-        int ans = 0;
-        isVisited= new boolean[computers.length];
+        
+        cnt = 0;
+        isVisited = new boolean[computers.length];
         
         for(int i=0;i<computers.length;i++){
             if(!isVisited[i]){
-                ans++;
                 dfs(i,computers);
+                cnt++;
             }
         }
         
-        return ans;
+        return cnt;
+        
     }
     
-    public static void dfs(int num,int[][] computers){
-        
-        isVisited[num] = true;
+    public static void dfs(int num, int[][] computers){
+        isVisited[num]=true;
         
         for(int i=0;i<computers.length;i++){
-            if(computers[num][i]==1&&!isVisited[i]){
+            if(!isVisited[i]&&computers[num][i]==1){
                 dfs(i,computers);
             }
         }
-        
-        
     }
 }
