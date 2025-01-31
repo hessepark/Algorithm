ae4fe9bfcf08a1ef491efbaab882d8237e1427da
hessepark <78264208+hessepark@users.noreply.github.com>
2025-01-31 09:50:51 +0900

    [level 3] Title: 아이템 줍기, Time: 1.31 ms, Memory: 84 MB -BaekjoonHub

--- 프로그래머스/3/87694. 아이템 줍기/아이템 줍기.java
+++ 프로그래머스/3/87694. 아이템 줍기/아이템 줍기.java
@@ -1,91 +1,86 @@
 import java.util.*;
 import java.io.*;
 
-//BFS
-//1.맵 2배 확장
-//2.갈 수 있는 미로 맵 만들기 (상하 반전)
-//3.visited배열도 두 배
-
-
-class Solution {
+class Solution { // BFS, 넓이를 두 배로 늘린다. 
     
-    public static int dx[]={0,1,0,-1};
-    public static int dy[]={-1,0,1,0};
-    
-    public static int visited[][];
-    public static boolean maze[][];
+    public static int dx[] = {0,1,0,-1};
+    public static int dy[] = {-1,0,1,0};
     
     public int solution(int[][] rectangle, int characterX, int characterY, int itemX, int itemY) {
         int answer = 0;
         
-        visited = new int[102][102];
-        maze = new boolean[102][102];
+        boolean[][] line = new boolean[102][102];
+        int[][] visited = new int[102][102];
         
-        for(int data[]:rectangle){
-            for(int i=data[1]*2;i<=data[3]*2;i++){
-                for(int j=data[0]*2;j<=data[2]*2;j++){
-                    maze[i][j]=true;
+        
+        //갈 수 있는 길이 true로
+        
+        for(int [] rect: rectangle) {
+            for(int i=rect[1]*2;i<=rect[3]*2;i++){
+                for(int j=rect[0]*2;j<=rect[2]*2;j++){
+                    line[i][j] = true;
                 }
             }
         }
-         for(int data[]:rectangle){
-              for(int i=data[1]*2+1;i<data[3]*2;i++){
-                for(int j=data[0]*2+1;j<data[2]*2;j++){
-                    maze[i][j]=false;
+        
+        for(int [] rect: rectangle) { //안 쪽 다 false 처리 (테두리만 남김) 여기다 +1 안 해줘서 문제였음
+            for(int i=rect[1]*2+1;i<rect[3]*2;i++){
+                for(int j=rect[0]*2+1;j<rect[2]*2;j++){
+                    line[i][j] = false;
                 }
             }
-         }
+        }
         
-        bfs(rectangle, characterX, characterY,itemX,itemY);
+        int cx=characterX*2;
+        int cy=characterY*2;
         
-        //System.out.println(visited[itemY*2][itemX*2]);
+        int ix=itemX*2;
+        int iy=itemY*2;
         
-        return visited[itemY*2][itemX*2]/2;
-    }
-    
-    public static void bfs(int[][] rectangle, int characterX, int characterY,int itemX,int itemY){
-        
-        int startX=characterX*2;
-        int startY=characterY*2;
-        int endX=itemX*2;
-        int endY=itemY*2;
-        
-        ArrayDeque<Point>q=new ArrayDeque<>();
-        q.add(new Point(startX,startY));
-        visited[startY][startX]=0;
-        
+        ArrayDeque<Point> q = new ArrayDeque<>();
+        q.add(new Point(cy,cx));
+        visited[cy][cx]=1;
         
         while(!q.isEmpty()){
-        
-        Point now = q.poll();    
+            Point now = q.poll();
             
-        for(int i=0;i<4;i++){
-            int nx= now.x+dx[i];
-            int ny= now.y+dy[i];
-            
-            if(nx==0||ny==0||nx==102||ny==102){
-                continue;
-            }
-            
-            if(maze[ny][nx]&&visited[ny][nx]==0){
-                q.add(new Point(nx,ny));
+            for(int i=0;i<4;i++){
+                int nx= dx[i]+now.x;
+                int ny= dy[i]+now.y;
+                
+                if(nx==0||ny==0||nx==102||ny==102||visited[ny][nx]!=0||line[ny][nx]==false){
+                    continue;
+                }
+                
                 visited[ny][nx]=visited[now.y][now.x]+1;
+                q.add(new Point(ny,nx));
             }
-            
-        }
-            
         }
         
+//         for(int i=0;i<102;i++){
+//             for(int j=0;j<102;j++){
+//                 if(line[i][j]){
+//                     System.out.print("T");
+//                 }
+//                 else{
+//                     System.out.print("F");
+//                 }
+                
+//             }
+//             System.out.println();
+//         }
+        
+        answer=visited[iy][ix]/2;
+        
+        return answer;
     }
-    
 }
 
 class Point {
-    int x;
     int y;
-    
-    public Point(int x,int y){
-        this.x=x;
+    int x;
+    public Point(int y, int x){
         this.y=y;
+        this.x=x;
     }
 }
