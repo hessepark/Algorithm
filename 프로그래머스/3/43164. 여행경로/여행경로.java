4015a2429fba21263c88d5f18bc2638cb303a5f2
hessepark <78264208+hessepark@users.noreply.github.com>
2025-01-31 11:01:44 +0900

    [level 3] Title: 여행경로, Time: 0.58 ms, Memory: 81.6 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.java
+++ 프로그래머스/3/43164. 여행경로/여행경로.java
@@ -1,74 +1,68 @@
 import java.util.*;
-import java.io.*; 
+import java.io.*;
 
 class Solution {
     
-    public static ArrayList<String>answer;
-    public static boolean isVisited[];
+    public static ArrayList<String>list = new ArrayList<>();
+    public static boolean[] isVisited;
     
     public String[] solution(String[][] tickets) {
         
-        answer = new ArrayList<>();
-        isVisited = new boolean[tickets.length];
+        String[] answer = {};
         
-        Arrays.sort(tickets,(a,b)-> {
-          if(a[0].compareTo(b[0])==0){ //출발지가 같으면
-              return a[1].compareTo(b[1]); //도착지 기준 오름차순
-          } 
-          else{
-              return a[0].compareTo(b[0]); //출발지 다르면 출발지 오름차순
-          }
+        isVisited=new boolean[tickets.length];
+        
+        Arrays.sort(tickets, (o1,o2)->{
+                   if(o1[0].compareTo(o2[0])==0){
+                       return o1[1].compareTo(o2[1]);
+                   }
+                   return o1[0].compareTo(o2[0]);
         });
         
-//         for(int i=0;i<tickets.length;i++){
-//             for(int j=0;j<tickets[0].length;j++){
-//                 System.out.print(tickets[i][j]+" ");
-//             }
-//             System.out.println();
-//         }
-        
-//         System.out.println("도착지 기준");
-        
-//         Arrays.sort(tickets,(a,b)-> {
-//           if(a[1].compareTo(b[1])==0){ //도착지가 같으면
-//               return a[0].compareTo(a[0]); //출발지 기준 오름차순
-//           } 
-//           else{
-//               return a[1].compareTo(b[1]); //도착지 다르면 도착지 오름차순
-//           }
-//         });
-        
-//         for(int i=0;i<tickets.length;i++){
-//             for(int j=0;j<tickets[0].length;j++){
-//                 System.out.print(tickets[i][j]+" ");
-//             }
-//             System.out.println();
-//         }
-        answer.add("ICN");
-        dfs("ICN",tickets);
-        
-        
-        return answer.toArray(new String[0]);
-    }
-    
-    public static void dfs(String cur,String [][] tickets){
-        
-        // if(answer.size()==tickets.length+1){
-        //     return;
+        // for(int i=0;i<tickets.length;i++){
+        //     System.out.println(tickets[i][0]+" "+tickets[i][1]);
         // }
         
-        for(int i=0;i<tickets.length;i++){
-            if(!isVisited[i]&&tickets[i][0].equals(cur)){
-                isVisited[i]=true;
-                answer.add(tickets[i][1]);
-                dfs(tickets[i][1],tickets);
-                   if(answer.size()==tickets.length+1){
+        list.add("ICN");
+        dfs("ICN",tickets);
+        
+        answer = new String[list.size()];
+        
+        for(int i=0;i<answer.length;i++){
+            answer[i] = list.get(i);
+        }
+
+        
+        return answer;
+    }
+    
+    public static void dfs(String city,String[][] tickets){
+    
+        //System.out.println("나 list야"+list+"나 티켓 길이야"+tickets.length);
+        
+        if(list.size()==tickets.length+1){
+            //System.out.println("지금 리턴:");
             return;
         }
-                isVisited[i]=false;
-                answer.remove(answer.size()-1);
-            }
+        
+        for(int i=0;i<tickets.length;i++){
+            if(!isVisited[i]){
+                 if(city.equals(tickets[i][0])){
+                   isVisited[i]=true;
+                   list.add(tickets[i][1]);
+                   //System.out.println(list+" "+i); //
+                   dfs(tickets[i][1],tickets);
+                   if(list.size()==tickets.length+1){
+                     //  System.out.println("리턴 되는 중:"+list);
+                       return;
+                   }
+                   isVisited[i]=false;
+                   list.remove(list.size()-1);
+               }
+             }
         }
         
+        
     }
+    
 }
