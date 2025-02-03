2f8553a49d0cf4c6a63582cdfd02e2843b8d021a
hessepark <78264208+hessepark@users.noreply.github.com>
2025-02-04 00:06:27 +0900

    [level 3] Title: 여행경로, Time: 0.78 ms, Memory: 80.9 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.java
+++ 프로그래머스/3/43164. 여행경로/여행경로.java
@@ -3,66 +3,47 @@ import java.io.*;
 
 class Solution {
     
+    public static boolean isVisited[];
     public static ArrayList<String>list = new ArrayList<>();
-    public static boolean[] isVisited;
     
     public String[] solution(String[][] tickets) {
-        
         String[] answer = {};
         
         isVisited=new boolean[tickets.length];
         
-        Arrays.sort(tickets, (o1,o2)->{
-                   if(o1[0].compareTo(o2[0])==0){
-                       return o1[1].compareTo(o2[1]);
-                   }
-                   return o1[0].compareTo(o2[0]);
+        Arrays.sort(tickets,(o1,o2)->{
+            if(o1[0].compareTo(o2[0])==0){
+                return o1[1].compareTo(o2[1]);
+            }
+            else{
+                return o1[0].compareTo(o2[0]);
+            }
         });
-        
-        // for(int i=0;i<tickets.length;i++){
-        //     System.out.println(tickets[i][0]+" "+tickets[i][1]);
-        // }
         
         list.add("ICN");
         dfs("ICN",tickets);
         
-        answer = new String[list.size()];
-        
-        for(int i=0;i<answer.length;i++){
-            answer[i] = list.get(i);
-        }
-
-        
-        return answer;
+        return list.toArray(new String[0]);
     }
     
-    public static void dfs(String city,String[][] tickets){
-    
-        //System.out.println("나 list야"+list+"나 티켓 길이야"+tickets.length);
+    public static void dfs(String cur,String[][] tickets){
         
         if(list.size()==tickets.length+1){
-            //System.out.println("지금 리턴:");
             return;
         }
         
         for(int i=0;i<tickets.length;i++){
-            if(!isVisited[i]){
-                 if(city.equals(tickets[i][0])){
-                   isVisited[i]=true;
-                   list.add(tickets[i][1]);
-                   //System.out.println(list+" "+i); //
-                   dfs(tickets[i][1],tickets);
-                   if(list.size()==tickets.length+1){ //이게 없으면 할 수 있는 조합 다 해봐야 끝난다.
-                     //  System.out.println("리턴 되는 중:"+list);
-                       return;
-                   }
-                   isVisited[i]=false;
-                   list.remove(list.size()-1); 
-               }
-             }
+            if(!isVisited[i]&&tickets[i][0].equals(cur)){
+                list.add(tickets[i][1]);
+                isVisited[i]=true;
+                dfs(tickets[i][1],tickets);
+                if(list.size()==tickets.length+1){
+                    return;
+                }
+                isVisited[i]=false;
+                list.remove(list.size()-1);
+            }
         }
         
-        
     }
-    
 }
