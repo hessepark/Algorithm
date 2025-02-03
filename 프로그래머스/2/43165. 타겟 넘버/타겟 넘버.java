a9b8b4ddb8964b4168b2962f92c3773dda82e676
hessepark <78264208+hessepark@users.noreply.github.com>
2025-02-04 01:05:34 +0900

    [level 2] Title: 타겟 넘버, Time: 4.37 ms, Memory: 77.5 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -2,35 +2,27 @@ import java.util.*;
 import java.io.*;
 
 class Solution {
-    
     public static int ans;
-    
     public int solution(int[] numbers, int target) {
-        
-        ans = 0;
+        ans=0;
         
         perm(0,0,numbers,target);
         
         return ans;
-        
     }
     
-    public static void perm(int cur,int depth,int[] numbers, int target){
-        
-     
+    public static void perm(int cur,int depth,int[] numbers,int target){
         
         if(depth==numbers.length){
-              if(cur==target){
-              ans++;
+            if(cur==target){
+                ans++;
             }
-            
             return;
         }
         
         perm(cur+numbers[depth],depth+1,numbers,target);
         
         perm(cur-numbers[depth],depth+1,numbers,target);
-        
         
     }
 }
