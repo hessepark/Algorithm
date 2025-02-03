2f170c188b657c52bff385448a260e6978bf70d1
hessepark <78264208+hessepark@users.noreply.github.com>
2025-02-04 03:32:02 +0900

    [Gold IV] Title: 회사 문화 1, Time: 1900 ms, Memory: 272376 KB -BaekjoonHub

--- 백준/Gold/14267. 회사 문화 1/회사 문화 1.java
+++ 백준/Gold/14267. 회사 문화 1/회사 문화 1.java
@@ -3,62 +3,57 @@ import java.util.Scanner;
 
 class Main {
 
-	// 트리 만들고 내리칭찬하기
-
 	public static int n, m;
-
 	public static ArrayList<Integer> list[];
-	//public static boolean[] isVisited;
-	public static int[] ans;
+	public static boolean isVisited[];
+	public static int praise[];
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-
 		n = sc.nextInt();
 		m = sc.nextInt();
 
 		list = new ArrayList[n + 1];
-
-		ans = new int[n + 1];
+		isVisited = new boolean[n + 1];
+		praise = new int[n + 1];
 
 		for (int i = 1; i < n + 1; i++) {
 			list[i] = new ArrayList<>();
 		}
 
-		//isVisited = new boolean[n + 1];
-
-		for (int i = 1; i < n + 1; i++) {
+		for (int i = 0; i < n; i++) {
 			int n = sc.nextInt();
-			if (i == 1)
+			if (n == -1) {
 				continue;
-			list[n].add(i);
-		}
+			}
 
-//		for(int i=1;i<n+1;i++) {
-//			System.out.println(list[i]);
-//		}
+			list[n].add(i + 1);
+			list[i + 1].add(n);
+
+		}
 
 		for (int i = 0; i < m; i++) {
-			int num = sc.nextInt();
+			int n = sc.nextInt();
 			int w = sc.nextInt();
-			
-			ans[num]+=w;
+			praise[n] += w;
 		}
-		
-		dfs(1);
 
-		for (int i = 1; i < n + 1; i++) {
-			System.out.print(ans[i] + " ");
+		dfs(1);
+		
+		for(int i=1;i<=n;i++) {
+			System.out.print(praise[i]+" ");
 		}
 
 	}
 
 	public static void dfs(int num) {
-
-		for (int next : list[num]) {
-			ans[next]+=ans[num];
-			dfs(next);
+		isVisited[num] = true;
+		for(int child:list[num]) {
+			if(!isVisited[child]) {
+				isVisited[child]=true;
+				praise[child]+=praise[num];
+				dfs(child);
+			}
 		}
-
 	}
 }
