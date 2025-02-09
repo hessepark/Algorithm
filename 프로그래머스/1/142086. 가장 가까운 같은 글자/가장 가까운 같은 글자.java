32ee5d9f619519bc6aec4c83ac2ff7c53556d74c
hessepark <78264208+hessepark@users.noreply.github.com>
2025-02-09 22:16:57 +0900

    [level 1] Title: 가장 가까운 같은 글자, Time: 101.81 ms, Memory: 85.1 MB -BaekjoonHub

--- 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.java
+++ 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.java
@@ -6,11 +6,21 @@ class Solution {
         HashMap<Character, Integer> map = new HashMap<>(); // 문자별 가장 최근 인덱스를 저장
 
        for(int i=0; i<s.length();i++){
-        char a = s.charAt(i);
-           
-           answer[i] = i - map.getOrDefault(a,i+1);
-           
-           map.put(a,i);
+            char a = s.charAt(i);
+            int idx=-1;
+           for(int j=0;j<i;j++){
+               char b = s.charAt(j);
+               
+               if(a==b){
+                   idx=j;
+               }
+           }
+           if(idx!=-1){
+               answer[i]=i-idx;
+           }
+           else{
+               answer[i]=-1;
+           }
        }
 
         return answer;
