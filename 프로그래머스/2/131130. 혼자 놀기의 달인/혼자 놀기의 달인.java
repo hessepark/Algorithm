f6bde6afe57bb8caed3a928955ea90f473fa8440
hessepark <78264208+hessepark@users.noreply.github.com>
2025-02-13 15:11:21 +0900

    [level 2] Title: 혼자 놀기의 달인, Time: 0.69 ms, Memory: 76 MB -BaekjoonHub

--- 프로그래머스/2/131130. 혼자 놀기의 달인/혼자 놀기의 달인.java
+++ 프로그래머스/2/131130. 혼자 놀기의 달인/혼자 놀기의 달인.java
@@ -1,34 +1,48 @@
 import java.util.*;
+import java.io.*;
 
 class Solution {
+    
+    public static boolean isVisited[];
+    public static ArrayList<Integer> list;
+    
     public int solution(int[] cards) {
         int answer = 0;
-        boolean[] isVisited = new boolean[cards.length + 1];
-
-        PriorityQueue<Integer> q = new PriorityQueue<>((o1, o2) -> Integer.compare(o2, o1));
-
-        for (int i = 0; i < cards.length; i++) {
-            if (isVisited[cards[i]]) {
+        
+        list = new ArrayList<>();
+        isVisited = new boolean[cards.length+1];
+        
+        for(int i=0;i<cards.length;i++){
+            if(isVisited[cards[i]]){
                 continue;
             }
-
+            
             int cur = cards[i];
-            int cnt = 0;
-
-            while (!isVisited[cur]) {
+            int cnt=0;
+            
+            while(!isVisited[cur]){
+            
                 cnt++;
-                isVisited[cur] = true;
-                cur = cards[cur - 1]; 
+                isVisited[cur]=true;
+                
+                cur=cards[cur-1];
+                
+                
             }
-            q.add(cnt);
+            
+            list.add(cnt);
+            
         }
-
-        if (q.size() < 2) { 
+        
+        Collections.sort(list,Collections.reverseOrder());
+        
+        System.out.println(list);
+        
+        if(list.size()<2){
             return 0;
         }
-
-        answer = q.poll() * q.poll();
-
-        return answer;
+        
+        return list.get(0)*list.get(1);
+        
     }
 }
