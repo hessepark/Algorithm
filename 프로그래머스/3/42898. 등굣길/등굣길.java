bab9c330086ec63971a8c1c107acd1e4a12cda74
hessepark <78264208+hessepark@users.noreply.github.com>
2025-03-06 00:12:19 +0900

    [level 3] Title: 등굣길, Time: 0.77 ms, Memory: 54.1 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/등굣길.java
+++ 프로그래머스/3/42898. 등굣길/등굣길.java
@@ -6,37 +6,43 @@ class Solution {
         int answer = 0;
         
         int dp[][]=new int[n+1][m+1] ; //1부터 시작
-        
-        for(int[]puddle:puddles){
-            dp[puddle[1]][puddle[0]]=-1;
+        for(int i=0;i<=m;i++){
+            dp[0][i]=Integer.MAX_VALUE;
         }
-
-        dp[1][1]=1;
-
+        
+        for(int i=0;i<puddles.length;i++){
+            dp[puddles[i][1]][puddles[i][0]]=-1;
+        }
+        
         for(int i=1;i<=n;i++){
             for(int j=1;j<=m;j++){
                 
-                if(i==1&&j==1) continue;
+                if(dp[i][j]==-1) continue;
                 
-                if(dp[i][j]==-1){
-                    dp[i][j]=0;
-                    continue;
+                if(dp[i-1][j]==-1){
+                    dp[i][j]=(dp[i][j-1]+1)%1000000007;
                 }
+                else if(dp[i][j-1]==-1){
+                    dp[i][j]=(dp[i-1][j]+1)%1000000007;
+                }
+                else{
+                    dp[i][j]=(Math.min(dp[i-1][j],dp[i][j-1])+1)%1000000007;
+                    }
                 
-                if(j>1) dp[i][j]= (dp[i][j]+dp[i][j-1])%1000000007;
-                if(i>1) dp[i][j]= (dp[i][j]+dp[i-1][j])%1000000007;
-            
+              //if(dp[n-1][m]!=0||dp[n][m-1]!=0){ //또는 그냥 인덱스로 보기
+                if(i==n-1&&j==m||i==n&&j==m-1){
+                  for(int k=1;k<=n;k++){
+            for(int z=1;z<=m;z++){
+                System.out.print(dp[k][z]);
             }
+            System.out.println();
         }
-        
-//         for(int i=1;i<=n;i++){
-//             for(int j=1; j<=m;j++){
-//                 System.out.print(dp[i][j]);
-//             }
-//             System.out.println();//
-//         }
-        
-        return dp[n][m];
+                  answer=dp[i][j];
+                 return answer-1;
+              }
+        }
+        }
+        return 0;
         
     }
 }
