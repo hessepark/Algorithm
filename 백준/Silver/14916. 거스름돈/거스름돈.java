0ff79fe3643d0ab2ced0810d97d4dea592eb5e78
hessepark <78264208+hessepark@users.noreply.github.com>
2025-03-13 01:01:07 +0900

    [Silver V] Title: 거스름돈, Time: 180 ms, Memory: 18072 KB -BaekjoonHub

--- 백준/Silver/14916. 거스름돈/거스름돈.java
+++ 백준/Silver/14916. 거스름돈/거스름돈.java
@@ -8,30 +8,42 @@ class Main {
 		int n = sc.nextInt();
 
 		int dp[] = new int[n + 5];
+		
+		for(int i=1;i<dp.length;i++) {
+			dp[i]=Integer.MAX_VALUE;
+		}
 
 		dp[2] = 1;
 		dp[5] = 1;
+		
+		int arr[]= {2,5};
 
 		for (int i = 1; i <= n; i++) {
-			
 
-			if (i - 2 >= 0 && dp[i - 2] != 0) {
-				dp[i] = dp[i - 2] + 1;
+//			if (i - 2 >= 0 && dp[i - 2] != Integer.MAX_VALUE) {
+//				dp[i] =Math.min(dp[i],dp[i - 2] + 1);
+//			}
+//
+//			if (i - 5 >= 0 && dp[i - 5] != Integer.MAX_VALUE) {
+//				dp[i] = Math.min(dp[i], dp[i - 5] + 1);
+//			}
+			
+			for(int j=0;j<arr.length;j++) {
+				if (i- arr[j] >=0 && dp[i-arr[j]]!=Integer.MAX_VALUE) {
+					dp[i] = Math.min(dp[i], dp[i-arr[j]]+1);
+				}
 			}
 
-			if (i - 5 >= 0 && dp[i - 5] != 0) {
-				dp[i] = Math.min(dp[i], dp[i - 5] + 1);
-			}
-			
-			//System.out.println(i+": "+dp[i]);
+			// System.out.println(i+": "+dp[i]);
 
 		}
+
+//		if (dp[n] == Integer.MAX_VALUE) {
+//			System.out.println(-1);
+//		} else {
+//			System.out.println(dp[n]);
+//		}
 		
-		if(dp[n]==0) {
-			System.out.println(-1);
-		}
-		else
-		System.out.println(dp[n]);
-
+		System.out.println(dp[n]==Integer.MAX_VALUE?-1:dp[n]);
 	}
 }
