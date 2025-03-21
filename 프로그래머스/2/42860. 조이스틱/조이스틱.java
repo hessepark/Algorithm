f39217d9040e398d4d6bbdebcf184c65a19a4d16
hessepark <78264208+hessepark@users.noreply.github.com>
2025-03-21 20:27:31 +0900

    [level 2] Title: 조이스틱, Time: 0.07 ms, Memory: 71.2 MB -BaekjoonHub

--- 프로그래머스/2/42860. 조이스틱/조이스틱.java
+++ 프로그래머스/2/42860. 조이스틱/조이스틱.java
@@ -1,6 +1,26 @@
+import java.util.*;
+import java.io.*; 
+
 class Solution {
     public int solution(String name) {
         int answer = 0;
+        
+         int length=name.length();
+        int move = length-1;
+       
+        
+        for(int x=0;x<name.length();x++){
+            answer+=Math.min(name.charAt(x)-'A','Z'-name.charAt(x)+1);
+            
+            int y = x+1;
+            while(y<length&&name.charAt(y)=='A'){
+                y++;
+            }
+            
+            move=Math.min(move,Math.min(x+x+length-y,length-y+length-y+x));
+        }
+        answer+=move;
+        
         return answer;
     }
 }
