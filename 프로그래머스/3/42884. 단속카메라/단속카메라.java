e1f5b509ecc939db0450d71fa90765fa67bd890e
hessepark <78264208+hessepark@users.noreply.github.com>
2025-03-23 22:35:55 +0900

    [level 3] Title: 단속카메라, Time: 8.48 ms, Memory: 60.5 MB -BaekjoonHub

---
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.io.*;
+
+class Solution {
+    public int solution(int[][] routes) {
+        int answer = 0;
+        
+        Arrays.sort(routes,(o1,o2)->{return Integer.compare(o1[1],o2[1]);});
+        
+        int camera=routes[0][1];
+        answer++;
+        
+        for(int i=0;i<routes.length;i++){
+            if(routes[i][0]>camera){
+                camera=routes[i][1];
+                answer++;
+            }
+        }
+        
+        return answer;
+    }
+}
