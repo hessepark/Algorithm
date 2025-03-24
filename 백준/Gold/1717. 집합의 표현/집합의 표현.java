afea4b4e2fde6d40b0fe2afc7ec01debfeb4d660
hessepark <78264208+hessepark@users.noreply.github.com>
2025-03-24 19:41:08 +0900

    [Gold V] Title: 집합의 표현, Time: 1676 ms, Memory: 249712 KB -BaekjoonHub

--- 백준/Gold/1717. 집합의 표현/집합의 표현.java
+++ 백준/Gold/1717. 집합의 표현/집합의 표현.java
@@ -3,30 +3,31 @@ import java.util.Scanner;
 
 class Main {
 	
-	public static int arr[];
+	public static int parent[];
 	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
 		int m = sc.nextInt();
 		
-		arr=new int[n+1];
+		parent=new int[n+1];
 		
 		for(int i=1;i<n+1;i++) {
-			arr[i]=i;
+			parent[i] = i;
 		}
 		
 		for(int i=0;i<m;i++) {
-			int num =sc.nextInt();
+			int num = sc.nextInt();
 			int a = sc.nextInt();
 			int b = sc.nextInt();
+			
+			
 			
 			if(num==0) {
 				union(a,b);
 			}
 			else {
-				boolean result = checkSame(a,b);
-				if(result) {
+				if(checkSame(a,b)) {
 					System.out.println("YES");
 				}
 				else {
@@ -34,37 +35,37 @@ class Main {
 				}
 			}
 			
-			//System.out.println(Arrays.toString(arr));
-		}
-	}
-
-
-	private static void union(int a, int b) {
-		a = find(a);
-		b = find(b);
-		
-		if(a>b) {
-			arr[a]=b;
-		}
-		else {
-			arr[b]=a;
+			//System.out.println(Arrays.toString(parent));
+			
 		}
 		
 	}
 	
-	private static int find(int n) {
-		if(arr[n]==n) {
+	public static void union(int a,int b) {
+		a = find(a);
+		b = find(b);
+		
+		//System.out.println("a:"+a+","+"b:"+b);
+		
+		if(a<b) {
+			parent[b]=a;
+		}
+		else {
+			parent[a]=b;
+		}
+	}
+	
+	public static int find(int n) {
+		if(parent[n]==n) {
 			return n;
 		}
-		return arr[n]=find(arr[n]);
+		return parent[n]= find(parent[n]);
 	}
-
-
-	private static boolean checkSame(int a, int b) {
+	
+	public static boolean checkSame(int a,int b) {
 		if(find(a)==find(b)) {
 			return true;
 		}
 		return false;
 	}
-
 }
