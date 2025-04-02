f7cd079be0e102f3f6fb766d46beb7c1ca177c77
hessepark <78264208+hessepark@users.noreply.github.com>
2025-04-02 23:02:46 +0900

    [level 2] Title: 구명보트, Time: 11.51 ms, Memory: 57.7 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.java
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -8,9 +8,9 @@ class Solution {
         Arrays.sort(people);
         
         int start=0;
-        int end = people.length-1;
+        int end=people.length-1;
         
-        while(start<=end){
+        while(start<=end) {
             if(people[start]+people[end]<=limit){
                 start++;
             }
