10a7dbdc0106d8fdfb34bd846bc2555d53fc1b3d
hessepark <78264208+hessepark@users.noreply.github.com>
2025-04-03 00:47:29 +0900

    [level 3] Title: 섬 연결하기, Time: 2.23 ms, Memory: 78.2 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
@@ -1,78 +1,80 @@
 import java.util.*;
 import java.io.*;
+
 class Solution {
     
     public static ArrayList<Integer> list[];
-    public static boolean[] isVisited;
-    public static int cnt;
+    public static boolean isVisited[];
     
     public int solution(int n, int[][] costs) {
         int answer = 0;
         
-        Arrays.sort(costs, (o1,o2)->Integer.compare(o2[2],o1[2]));
-        
         list = new ArrayList[n];
+        isVisited = new boolean[n];
         
         for(int i=0;i<n;i++){
             list[i] = new ArrayList<>();
         }
         
-        for(int i=0;i<costs.length;i++) {
-            int a = costs[i][0];
-            int b = costs[i][1];
-            int v = costs[i][2];
-            
-            list[a].add(b);
-            list[b].add(a);
-            answer+=v;
-        }
-                  
-        int except=0;
+        Arrays.sort(costs,((o1,o2)->Integer.compare(o2[2],o1[2])));
         
         for(int i=0;i<costs.length;i++){
-              
-            if(costs.length-except==n-1){
-                break;
-            }
+            
+            answer+=costs[i][2];
             
             int a = costs[i][0];
             int b = costs[i][1];
-            int v = costs[i][2];
+            
+            list[a].add(b);
+            list[b].add(a); 
+        }
+        
+        int cnt=0;
+        
+        for (int i=0;i<costs.length;i++){
+            
+            if(costs.length-cnt==n-1){
+                break;
+            }
+                 
+            int a = costs[i][0];
+            int b = costs[i][1];
             
             list[a].remove(Integer.valueOf(b));
             list[b].remove(Integer.valueOf(a));
             
             isVisited=new boolean[n];
-            cnt=0;
             
-            dfs(0);
+            int connect=dfs(0);
             
-            //n이 4면 최소 3개는 봐야함
-            if(cnt!=n){
+            if(connect!=n){
                 list[a].add(b);
                 list[b].add(a);
             }
             else{
-                answer-=v;
-                except++;
+                answer-=costs[i][2];
+                cnt++;
             }
             
-            
         }
+        
         
         return answer;
     }
     
-    public static void dfs(int num){
+    public static int dfs(int n){
         
-        isVisited[num]=true;
-        cnt++;
+        int cnt=1;
+        isVisited[n] = true;
         
-        for(int child:list[num]){
+        for(int child:list[n]){
             if(!isVisited[child]){
-                dfs(child);
+                cnt+=dfs(child);
             }
         }
+        
+        return cnt;
+        
         
     }
 }
