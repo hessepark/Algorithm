edf96b4e538ed4c8497c8df45048c218e60abacf
hessepark <78264208+hessepark@users.noreply.github.com>
2025-04-03 02:10:59 +0900

    [level 3] Title: 단속카메라, Time: 9.26 ms, Memory: 55.7 MB -BaekjoonHub

--- 프로그래머스/3/42884. 단속카메라/단속카메라.java
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.java
@@ -1,41 +1,43 @@
 import java.util.*;
+import java.io.*;
 
 class Solution {
     public int solution(int[][] routes) {
-        boolean[] isVisited = new boolean[routes.length];
         int answer = 0;
         
-        Arrays.sort(routes, (a, b) -> { // 1. 진입점 오름차순 정렬, 2. 나간점 오름차순 정렬
-            // if (a[0] == b[0]) {
-            //     return Integer.compare(a[1], b[1]);
-            // }
-            return Integer.compare(a[0], b[0]);
-        });
+        Arrays.sort(routes,(o1,o2)->Integer.compare(o1[0],o2[0]));
         
-        for (int i = 0; i < routes.length; i++) {
-            if (isVisited[i]) {
+        boolean isVisited[]=new boolean[routes.length];
+        
+        for(int i=0;i<routes.length;i++){
+            
+            if(isVisited[i]){
                 continue;
             }
             
-            int start = routes[i][0];
-            int end = routes[i][1];
-            isVisited[i] = true;
+            isVisited[i]=true;
             
-            for (int j = i + 1; j < routes.length; j++) {
-                if (isVisited[j]) {
-                    continue;
+            int start=routes[i][0];
+            int end=routes[i][1];
+            
+            //같이 찍을 수 있으면 true로 만들기
+            
+            for(int j=i+1;j<routes.length;j++) {
+                
+                if(end<routes[j][0]){
+                    break;
                 }
                 
-                if (end < routes[j][0]) { // 방문했거나 범위 밖이면 종료
-                    continue;
-                }
+                start=Math.max(start,routes[j][0]);
+                end=Math.min(end,routes[j][1]);
                 
-                start = Math.max(start, routes[j][0]);
-                end = Math.min(end, routes[j][1]);
-                isVisited[j] = true;
+                isVisited[j]=true;
+                
             }
             
+            
             answer++;
+            
         }
         
         return answer;
