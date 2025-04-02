fc42a844522d212d02de5424c3fa67200a8691ee
hessepark <78264208+hessepark@users.noreply.github.com>
2025-04-03 01:56:34 +0900

    [level 3] Title: 단속카메라, Time: 17.79 ms, Memory: 60.4 MB -BaekjoonHub

--- 프로그래머스/3/42884. 단속카메라/단속카메라.java
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.java
@@ -1,20 +1,41 @@
 import java.util.*;
-import java.io.*;
 
 class Solution {
     public int solution(int[][] routes) {
+        boolean[] isVisited = new boolean[routes.length];
         int answer = 0;
         
-        Arrays.sort(routes,(o1,o2)->{return Integer.compare(o1[1],o2[1]);});
+        Arrays.sort(routes, (a, b) -> { // 1. 진입점 오름차순 정렬, 2. 나간점 오름차순 정렬
+            // if (a[0] == b[0]) {
+            //     return Integer.compare(a[1], b[1]);
+            // }
+            return Integer.compare(a[0], b[0]);
+        });
         
-        int camera=routes[0][1];
-        answer++;
-        
-        for(int i=1;i<routes.length;i++){
-            if(camera<routes[i][0]){
-                camera=routes[i][1];
-                answer++;
+        for (int i = 0; i < routes.length; i++) {
+            if (isVisited[i]) {
+                continue;
             }
+            
+            int start = routes[i][0];
+            int end = routes[i][1];
+            isVisited[i] = true;
+            
+            for (int j = i + 1; j < routes.length; j++) {
+                if (isVisited[j]) {
+                    continue;
+                }
+                
+                if (end < routes[j][0]) { // 방문했거나 범위 밖이면 종료
+                    continue;
+                }
+                
+                start = Math.max(start, routes[j][0]);
+                end = Math.min(end, routes[j][1]);
+                isVisited[j] = true;
+            }
+            
+            answer++;
         }
         
         return answer;
