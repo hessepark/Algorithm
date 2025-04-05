5ced08baaa65f096a002aba2b7926232ff035478
hessepark <78264208+hessepark@users.noreply.github.com>
2025-04-05 18:42:22 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 12.65 ms, Memory: 75.5 MB -BaekjoonHub

---
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
@@ -0,0 +1,38 @@
+import java.util.*;
+import java.io.*;
+
+class Solution {
+    public int solution(int[][] jobs) {
+        int answer = 0;
+        
+        //먼저 시간 순 정렬
+        Arrays.sort(jobs,(o1,o2)->Integer.compare(o1[0],o2[0]));
+        
+        //소요시간이 짧은 순서대로 큐에 들어가게 만듦
+        PriorityQueue<int[]>q = new PriorityQueue<>((o1,o2)->Integer.compare(o1[1],o2[1]));
+        
+        int time=0; //현재 시각
+        //현 시간부로 들어갈 수 있는 친구들 큐에 줄 세움
+        int idx=0; //현재 인덱스
+        int count=0;
+        
+        while(count<jobs.length){
+            while(idx<jobs.length&&jobs[idx][0]<=time){
+                    q.add(jobs[idx++]);
+                }
+            if(!q.isEmpty()){
+                int num[]=q.poll();
+                count++;
+                time+=num[1];
+                answer+=time-num[0];
+            }
+            else{
+                time++;
+            }
+            System.out.println(answer);
+        }
+        
+        
+        return answer/jobs.length;
+    }
+}
