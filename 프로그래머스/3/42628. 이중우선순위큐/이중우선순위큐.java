6237bd0c1757c17eb7afab77387f670dc5b49560
hessepark <78264208+hessepark@users.noreply.github.com>
2025-04-05 15:59:15 +0900

    [level 3] Title: 이중우선순위큐, Time: 104.68 ms, Memory: 113 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
@@ -8,35 +8,30 @@ class Solution {
         PriorityQueue<Integer>minQ = new PriorityQueue<>();
         PriorityQueue<Integer>maxQ = new PriorityQueue<>(Collections.reverseOrder());
         
-       
-        
-        for(int i=0;i<operations.length;i++){
-             String oper[]= operations[i].split(" ");
-            String alpha=oper[0];
-            int num =Integer.parseInt(oper[1]);
+        for(int i=0; i<operations.length; i++){
+            String op[] = operations[i].split(" ");
+            
+            String alpha = op[0];
+            int num = Integer.parseInt(op[1]);
             
             if(alpha.equals("I")){
                 minQ.add(num);
                 maxQ.add(num);
             }
-            else{
-                
+            else {
                 if(minQ.isEmpty()){
                     continue;
                 }
                 
                 if(num<0){
-                int n=minQ.poll();
-                maxQ.remove(n);
+                    int n=minQ.poll();
+                    maxQ.remove(n);
                 }
                 else{
                     int n=maxQ.poll();
                     minQ.remove(n);
                 }
             }
-            
-             // System.out.println(minQ);
-             // System.out.println(maxQ);
         }
         
         if(!minQ.isEmpty()){
