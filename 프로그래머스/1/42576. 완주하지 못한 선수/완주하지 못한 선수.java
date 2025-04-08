2f2535088b04af931f0a0d88b9fe2876c0074a63
hessepark <78264208+hessepark@users.noreply.github.com>
2025-04-08 20:41:48 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 80.43 ms, Memory: 97.5 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -1,23 +1,23 @@
 import java.util.*;
 import java.io.*;
 
-class Solution { //다 넣고 홀수 인 애 추출
+class Solution {
     public String solution(String[] participant, String[] completion) {
         String answer = "";
         
-        HashMap<String,Integer>map = new HashMap<>();
+        HashMap<String, Integer> map = new HashMap<>();
         
         for(int i=0;i<participant.length;i++){
             map.put(participant[i],map.getOrDefault(participant[i],0)+1);
         }
         
         for(int i=0;i<completion.length;i++){
-            map.put(completion[i],map.get(completion[i])+1);
+            map.put(completion[i],map.getOrDefault(completion[i],0)+1);
         }
         
-        for(String person:map.keySet()){
-            if(map.get(person)%2!=0){
-                answer=person;
+        for(int i=0;i<participant.length;i++){
+            if(map.get(participant[i])%2!=0){
+                answer=participant[i];
                 break;
             }
         }
