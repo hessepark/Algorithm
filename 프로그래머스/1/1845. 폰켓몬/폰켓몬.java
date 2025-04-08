0f7134213eabee5f89feb57c53273eea1d805ff0
hessepark <78264208+hessepark@users.noreply.github.com>
2025-04-08 20:54:11 +0900

    [level 1] Title: 폰켓몬, Time: 8.78 ms, Memory: 89.1 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -5,22 +5,18 @@ class Solution {
     public int solution(int[] nums) {
         int answer = 0;
         
-        //해시맵 크기 < nums/2면 해시맵 크기
-        //해시맵 크기 > nums/2면 nums/2
+        HashMap<Integer,Integer>map = new HashMap<>();
         
-        HashMap<Integer,Integer> map = new HashMap<>();
-        
-        for(int i=0;i<nums.length;i++){
+        for(int i=0; i<nums.length; i++) {
             map.put(nums[i],map.getOrDefault(nums[i],0)+1);
         }
         
-        if(map.size()<nums.length/2){
-            answer=map.size();
-        }
-        else{
+        if(map.size()>nums.length/2){
             answer=nums.length/2;
         }
-        
+        else{
+            answer=map.size();
+        }
         
         return answer;
     }
