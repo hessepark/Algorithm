ee5f6ed20bdf64c9d76c3d8a9b1cb6f7adcd257b
hessepark <78264208+hessepark@users.noreply.github.com>
2025-04-18 13:54:40 +0900

    [level 2] Title: 올바른 괄호, Time: 13.42 ms, Memory: 54.9 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/README.md
+++ 프로그래머스/2/12909. 올바른 괄호/README.md
@@ -1,10 +1,10 @@
 # [level 2] 올바른 괄호 - 12909 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12909) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12909?language=java) 
 
 ### 성능 요약
 
-메모리: 54.8 MB, 시간: 12.95 ms
+메모리: 54.9 MB, 시간: 13.42 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 01월 28일 21:21:09
+2025년 04월 18일 13:54:36
 
 ### 문제 설명
 
--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -5,22 +5,22 @@ class Solution {
     boolean solution(String s) {
         boolean answer = true;
 
-        ArrayDeque<Character>q = new ArrayDeque<>();
+        ArrayDeque<String>q= new ArrayDeque<>();
         
         for(int i=0;i<s.length();i++){
             if(s.charAt(i)=='('){
-                q.add(s.charAt(i));
+                q.add("(");
             }
-            else{
+            else {
                 if(q.isEmpty()){
-                    return false; //바로 )부터 들어오면
+                    return false;
                 }
                 q.poll();
             }
         }
         
         if(!q.isEmpty()){
-            answer = false;
+            answer=false;
         }
 
         return answer;
