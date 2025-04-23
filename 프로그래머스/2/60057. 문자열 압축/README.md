5b9b029412b0289d256afa012111253f11465fb3
hessepark <78264208+hessepark@users.noreply.github.com>
2025-04-24 04:16:51 +0900

    [level 2] Title: 문자열 압축, Time: 8.20 ms, Memory: 75.5 MB -BaekjoonHub

--- 프로그래머스/2/60057. 문자열 압축/README.md
+++ 프로그래머스/2/60057. 문자열 압축/README.md
@@ -1,10 +1,10 @@
 # [level 2] 문자열 압축 - 60057 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/60057?gad_source=1&gclid=CjwKCAjw--K_BhB5EiwAuwYoyvumYgAjK-WDH3khaNwB07rFJYT6uszQiZhdRER0Wn4fdAHbOztTKBoCrL8QAvD_BwE) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/60057#qna) 
 
 ### 성능 요약
 
-메모리: 85.6 MB, 시간: 5.37 ms
+메모리: 75.5 MB, 시간: 8.20 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 04월 24일 03:55:08
+2025년 04월 24일 04:16:49
 
 ### 문제 설명
 
--- 프로그래머스/2/60057. 문자열 압축/문자열 압축.java
+++ 프로그래머스/2/60057. 문자열 압축/문자열 압축.java
@@ -5,46 +5,55 @@ class Solution {
     public int solution(String s) {
         int answer = s.length();
         
-        for(int i=1;i<=s.length()/2;i++){
-            answer = Math.min(answer,compress(s,i));
-        }
+        for(int l=1;l<=s.length()/2;l++){
+        
+             ArrayList<String> words = new ArrayList<>();
+                
+            for(int i=0;i<s.length();i+=l) {
+                     words.add(s.substring(i,Math.min(s.length(),i+l)));
+                }
+            
+            ArrayDeque<Entry>q = new ArrayDeque<>();
+            
+            q.add(new Entry(words.get(0),1));
+            
+            for(int i=1;i<words.size();i++){
+                if(q.peekLast().word.equals(words.get(i))){
+                    Entry entry = q.pollLast();
+                    q.add(new Entry(entry.word,entry.count+1));
+                }
+                else{
+                    q.add(new Entry(words.get(i),1));
+                }
+            }
+            
+            StringBuilder compressed = new StringBuilder();
+            
+            while(!q.isEmpty()){
+                Entry entry = q.pollLast();
+                if(entry.count>1){
+                    compressed.append(String.valueOf(entry.count));
+                }
+                compressed.append(entry.word);
+            }
+
+            answer=Math.min(answer,compressed.toString().length());
+            
+            
+            }
         
         return answer;
     }
     
-    public int compress(String s,int length) {
-        
-        ArrayList<String> words = new ArrayList<>();
-        for(int i=0;i<s.length();i+=length){
-            words.add(s.substring(i,Math.min(s.length(),i+length)));
-        }
-        StringBuilder sb = new StringBuilder();
-        int count=0;
-        String prev="";
-        
-        for(String word: words) {
-            if(word.equals(prev)){
-                count++;
-            }
-            else{
-                
-                if(count>1){
-                    sb.append(String.valueOf(count));
-                }
-                sb.append(prev);
-                
-                prev=word;
-                
-                count=1;
-            }
-        }
-        
-        if(count>1){
-                    sb.append(String.valueOf(count));
-                }
-        sb.append(prev);
-        
-        return sb.toString().length();
-        
+    
+}
+
+class Entry {
+    String word;
+    int count;
+    
+    public Entry(String word, int count){
+        this.word=word;
+        this.count=count;
     }
 }
