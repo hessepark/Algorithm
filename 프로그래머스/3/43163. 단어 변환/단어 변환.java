ff58847fbd87db9a3d2319f009a95beaa37d6806
hessepark <78264208+hessepark@users.noreply.github.com>
2025-04-25 15:26:55 +0900

    [level 3] Title: 단어 변환, Time: 0.90 ms, Memory: 76.9 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -2,66 +2,45 @@ import java.util.*;
 import java.io.*;
 
 class Solution {
-    
-    public static boolean isVisited[];
-    public static ArrayDeque<Word>q;
-    public static int answer;
-    
     public int solution(String begin, String target, String[] words) {
-        answer = 0;
+        int answer = 0;
         
-        isVisited = new boolean[words.length];
-        q = new ArrayDeque<>();
+        ArrayDeque<Word>q = new ArrayDeque<>();
+        boolean[] isVisited = new boolean[words.length];
         q.add(new Word(begin,0));
         
-        bfs(begin,target,words);
-        
-        return answer;
-    }
-    
-    public static void bfs(String cur, String target, String[] words){
-               
         while(!q.isEmpty()){
-        
-            Word now = q.poll();
+            Word word = q.poll();
             
-            if(now.word.equals(target)){
-                answer=now.cnt;
-                return;
+            if(word.word.equals(target)){
+                return word.cnt;
             }
             
             for(int i=0;i<words.length;i++){
-                if(!isVisited[i]&&isPossible(words[i],now.word)){
+                if(!isVisited[i]&&isPossible(words[i],word.word)){
+                    q.add(new Word(words[i],word.cnt+1));
+                    //System.out.println(words[i]+" "+(word.cnt+1));
                     isVisited[i]=true;
-                    q.add(new Word(words[i],now.cnt+1));
                 }
             }
             
         }
         
-        
-        
+        return answer;
     }
     
     public static boolean isPossible(String a,String b){
-        
         int cnt=0;
         
         for(int i=0;i<a.length();i++){
             if(a.charAt(i)!=b.charAt(i)){
                 cnt++;
             }
+            if(cnt>1) return false;
         }
         
-        if(cnt!=1){
-            return false;
-        }
-        else{
-            return true;
-        }
-        
+        return true;
     }
-    
 }
 
 class Word {
