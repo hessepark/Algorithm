61f6a7ac516606f48dcd6cd1a588f0afc6058b1f
hessepark <78264208+hessepark@users.noreply.github.com>
2025-05-27 22:50:52 +0900

    [level 1] Title: 신규 아이디 추천, Time: 12.79 ms, Memory: 78.8 MB -BaekjoonHub

--- 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.java
+++ 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.java
@@ -4,43 +4,44 @@ import java.io.*;
 class Solution {
     public String solution(String new_id) {
         
+        //1단계
+        new_id = new_id.toLowerCase();
+        
+        //2단계
         StringBuilder sb = new StringBuilder();
-        
-        new_id = new_id.toLowerCase(); //1단계
-        
-        
-        for(int i=0;i<new_id.length();i++){ //2단계
-            char c = new_id.charAt(i);
-            if((c>='a'&&c<='z')||(c>='0'&&c<='9')||c=='-'||c=='_'||c=='.'){
+        for(int i=0;i<new_id.length();i++){
+            char c= new_id.charAt(i);
+            if(c>='a'&&c<='z'||c>='0'&&c<='9'||c=='-'||c=='_'||c=='.'){
                 sb.append(c);
             }
         }
+        System.out.println(sb);
+        
+        //3단계
+        new_id=sb.toString();
+        sb=new StringBuilder();
+        
+        boolean isPeriod=false;
+        for(int i=0;i<new_id.length();i++){
+            char c=new_id.charAt(i);
+            if(c=='.'){
+                isPeriod=true;
+            }
+            else{
+                if(isPeriod==true){
+                    sb.append('.');
+                }
+                sb.append(c);
+                isPeriod=false;
+            }
+        }
+        System.out.println(sb);
         
         new_id=sb.toString();
         
-        sb = new StringBuilder();
-        
-        //3단계
-        boolean isFlag = false;
-        for(int i=0;i<new_id.length();i++){ // new_id.toCharArray()
-            char c = new_id.charAt(i);
-            if(c=='.'){
-                if(!isFlag){
-                    sb.append(c);
-                    isFlag=true;
-                }
-            }
-            else{
-                sb.append(c);
-                isFlag=false;
-            }
-        }
-        
-        new_id = sb.toString();
-        
         //4단계
         if(new_id.startsWith(".")){
-            new_id=new_id.substring(1);
+            new_id=new_id.substring(1,new_id.length());
         }
         
         if(new_id.endsWith(".")){
@@ -49,29 +50,29 @@ class Solution {
         
         //5단계
         if(new_id.length()==0){
-            new_id+="a";
+            new_id+='a';
         }
         
         //6단계
         if(new_id.length()>=16){
             new_id=new_id.substring(0,15);
-        }
-        
-        if(new_id.endsWith(".")){
-            new_id=new_id.substring(0,new_id.length()-1);
-        }
-        
-        //7단계
-        if(new_id.length()<=2){
-            char c = new_id.charAt(new_id.length()-1); //마지막 문자
             
-            while(new_id.length()<=2){
-                new_id+=c;
+            if(new_id.endsWith(".")){
+                 new_id=new_id.substring(0,new_id.length()-1);
             }
         }
         
-        return new_id;
+        //7단계
         
+        char c = new_id.charAt(new_id.length()-1);
+        while(new_id.length()<=2){
+            new_id+=c;
+        }
+        
+        
+        
+        
+        return new_id;
         
     }
 }
