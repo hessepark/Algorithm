c521a50499d1c2a17423fa66e6b9d735096a910b
hessepark <78264208+hessepark@users.noreply.github.com>
2025-05-29 17:07:31 +0900

    [level 2] Title: 주식가격, Time: 29.24 ms, Memory: 78.7 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -5,23 +5,20 @@ class Solution {
         int[] answer = new int[prices.length];
         
         ArrayDeque<Integer>q = new ArrayDeque<>();
-        q.add(0);
         
-        for(int i=1;i<prices.length;i++){
-            while(!q.isEmpty()){
-                if(prices[i]<prices[q.peekLast()]){
-                    int num=q.pollLast();
-                    answer[num]=i-num;
-                }
-                else{
-                    break;
-                }
+        for(int i=0;i<prices.length;i++){
+            while(!q.isEmpty()&&prices[i]<prices[q.peekLast()]){
+                int idx=q.peekLast();
+                answer[idx]=i-idx;
+                q.pollLast();
             }
-            q.add(i);
+            q.addLast(i);
         }
         
+        //System.out.println(q);
+        
         while(!q.isEmpty()){
-            int idx = q.pollLast();
+            int idx=q.pollLast();
             answer[idx]=prices.length-idx-1;
         }
         
