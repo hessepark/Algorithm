619f6ca6832711150e4523f07ca03e9a7137240b
hessepark <78264208+hessepark@users.noreply.github.com>
2025-05-29 16:35:33 +0900

    [level 2] Title: 기능개발, Time: 0.25 ms, Memory: 87.5 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -1,51 +1,46 @@
 import java.util.*;
 import java.io.*;
-
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
         int[] answer = {};
         
-        //작업 일수 담음
-        ArrayDeque<Integer>list = new ArrayDeque<>();
-        
-        for(int i=0;i<progresses.length;i++){
-            int num = (100-progresses[i]);
-            
-            if(num%speeds[i]==0){
-                list.add(num/speeds[i]);
-            }
-            else{
-                list.add(num/speeds[i]+1);
-            }
-            
-        }
-        
-        //System.out.println(list);
-        
-        
-        //빼내는 수 담음
-        
         ArrayList<Integer>result = new ArrayList<>();
         
-        while(!list.isEmpty()){
-            int num = list.pollFirst();
-            
-            int cnt=1;
-            
-         
-              while(!list.isEmpty()&&list.peekFirst()<=num){
-                    cnt++;
-                    list.pollFirst();
+        ArrayDeque<Integer>q = new ArrayDeque<>();
+        
+        for(int i=0;i<progresses.length;i++){
+            if((100-progresses[i])%speeds[i]==0){
+                q.add((100-progresses[i])/speeds[i]);
             }
-            result.add(cnt);
+            else{
+                q.add((100-progresses[i])/speeds[i]+1);
+            }
         }
+        
+        //System.out.println(q);
+        
+        while(!q.isEmpty()){
+            int count=1;
+            int num=q.poll();
+            while(!q.isEmpty()){
+                if(num>=q.peekFirst()){
+                    q.poll();
+                    count++;
+                }
+                else{
+                    break;
+                }
+            }
+            result.add(count);
+        }
+        
+        //System.out.println(result);
         
         answer=new int[result.size()];
         
         for(int i=0;i<result.size();i++){
             answer[i]=result.get(i);
         }
-        
         
         return answer;
     }
