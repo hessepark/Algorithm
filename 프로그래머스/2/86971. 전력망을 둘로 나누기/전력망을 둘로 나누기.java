afa5537f1fe2ecaca4d8816901d3505c3c505adc
hessepark <78264208+hessepark@users.noreply.github.com>
2025-05-30 02:05:52 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 0.40 ms, Memory: 79.7 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -1,24 +1,24 @@
 import java.util.*;
 import java.io.*;
 class Solution {
-    
     public static int n;
     public static int minDiff;
-    public static boolean[] isVisited;
     public static ArrayList<Integer>list[];
+    public static boolean isVisited[];
     public int solution(int n, int[][] wires) {
-       
+        
         this.n=n;
         minDiff=Integer.MAX_VALUE;
-        isVisited=new boolean[n+1];
+        
         list=new ArrayList[n+1];
         for(int i=1;i<n+1;i++){
             list[i]=new ArrayList<>();
         }
+        isVisited=new boolean[n+1];
         
         for(int i=0;i<wires.length;i++){
-            int a=wires[i][0];
-            int b=wires[i][1];
+            int a = wires[i][0];
+            int b = wires[i][1];
             list[a].add(b);
             list[b].add(a);
         }
@@ -32,18 +32,17 @@ class Solution {
         
         isVisited[num]=true;
         
-        int count=1;
+        int cnt=1;
         
         for(int child:list[num]){
             if(!isVisited[child]){
-                count+=dfs(child);
+                cnt+=dfs(child);
             }
         }
         
-        minDiff=Math.min(minDiff,Math.abs(n-count*2));
+        minDiff=Math.min(minDiff,Math.abs(n-cnt*2));
         
-        return count;
-        
+        return cnt;
         
     }
 }
