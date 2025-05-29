36ad093393330bf1645649b7fe31a26b1861213d
hessepark <78264208+hessepark@users.noreply.github.com>
2025-05-30 02:01:19 +0900

    [level 1] Title: 신규 아이디 추천, Time: 12.94 ms, Memory: 86.7 MB -BaekjoonHub

--- 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.java
+++ 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.java
@@ -4,42 +4,41 @@ import java.io.*;
 class Solution {
     public String solution(String new_id) {
         
-        //1단계
-        new_id = new_id.toLowerCase();
+        //1
+        new_id=new_id.toLowerCase();
         
-        //2단계
+        
+        //2
         StringBuilder sb = new StringBuilder();
-        for(int i=0;i<new_id.length();i++){
-            char c= new_id.charAt(i);
+        for(char c:new_id.toCharArray()){
             if(c>='a'&&c<='z'||c>='0'&&c<='9'||c=='-'||c=='_'||c=='.'){
                 sb.append(c);
             }
         }
-        System.out.println(sb);
         
-        //3단계
         new_id=sb.toString();
         sb=new StringBuilder();
         
         boolean isPeriod=false;
         for(int i=0;i<new_id.length();i++){
-            char c=new_id.charAt(i);
+            char c =new_id.charAt(i);
             if(c=='.'){
                 isPeriod=true;
             }
             else{
-                if(isPeriod==true){
+                if(isPeriod){
                     sb.append('.');
                 }
                 sb.append(c);
                 isPeriod=false;
             }
         }
-        System.out.println(sb);
         
         new_id=sb.toString();
+        sb=new StringBuilder();
         
-        //4단계
+        
+        //4
         if(new_id.startsWith(".")){
             new_id=new_id.substring(1,new_id.length());
         }
@@ -48,28 +47,24 @@ class Solution {
             new_id=new_id.substring(0,new_id.length()-1);
         }
         
-        //5단계
+        //5
         if(new_id.length()==0){
-            new_id+='a';
+            new_id+="a";
         }
         
-        //6단계
+        //6
         if(new_id.length()>=16){
             new_id=new_id.substring(0,15);
-            
-            if(new_id.endsWith(".")){
-                 new_id=new_id.substring(0,new_id.length()-1);
-            }
+                 if(new_id.endsWith(".")){
+            new_id=new_id.substring(0,new_id.length()-1);
+        }
         }
         
-        //7단계
-        
+        //7
         char c = new_id.charAt(new_id.length()-1);
         while(new_id.length()<=2){
             new_id+=c;
         }
-        
-        
         
         
         return new_id;
