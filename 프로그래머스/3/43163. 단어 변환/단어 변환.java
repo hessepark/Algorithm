4250460161215a33220e6e2e1ade50503de9c2b9
hessepark <78264208+hessepark@users.noreply.github.com>
2025-05-29 22:31:39 +0900

    [level 3] Title: 단어 변환, Time: 0.64 ms, Memory: 80.4 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -1,11 +1,15 @@
 import java.util.*;
 import java.io.*;
 class Solution {
+    
     public static boolean isVisited[];
+    
     public int solution(String begin, String target, String[] words) {
+        
+        isVisited=new boolean[words.length];
+        
         ArrayDeque<Word>q = new ArrayDeque<>();
         q.add(new Word(begin,0));
-        isVisited=new boolean[words.length];
         
         while(!q.isEmpty()){
             
@@ -16,12 +20,11 @@ class Solution {
             }
             
             for(int i=0;i<words.length;i++){
-                if(!isVisited[i]&&isCount(now.word,words[i])==1){
+                if(!isVisited[i]&&isCount(words[i],now.word)==1){
                     q.add(new Word(words[i],now.cnt+1));
                     isVisited[i]=true;
                 }
             }
-            
             
         }
         
