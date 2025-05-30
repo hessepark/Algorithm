db9395a4d0ed4825dc40a363198003886582f0e6
hessepark <78264208+hessepark@users.noreply.github.com>
2025-05-30 10:11:06 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 8.31 ms, Memory: 56.6 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,19 +1,17 @@
 import java.util.*;
 import java.io.*;
 class Solution {
-    
-    public static int dr[]={-1,0,1,0};
-    public static int dc[]={0,1,0,-1};
-    
+    public static int dr[] = {-1,0,1,0};
+    public static int dc[] = {0,1,0,-1};
     public static int visited[][];
-    
     public int solution(int[][] maps) {
+        
         int n = maps.length;
         int m = maps[0].length;
         
         visited=new int[n][m];
         
-        ArrayDeque<Pos>q= new ArrayDeque<>();
+        ArrayDeque<Pos>q = new ArrayDeque<>();
         q.add(new Pos(0,0));
         visited[0][0]=1;
         
@@ -26,16 +24,16 @@ class Solution {
             }
             
             for(int k=0;k<4;k++){
-                int nr=now.r+dr[k];
-                int nc=now.c+dc[k];
+                int nr = now.r+dr[k];
+                int nc = now.c+dc[k];
                 
                 if(nr<0||nc<0||nr>=n||nc>=m){
                     continue;
                 }
                 
                 if(visited[nr][nc]==0&&maps[nr][nc]==1){
-                    q.add(new Pos(nr,nc));
                     visited[nr][nc]=visited[now.r][now.c]+1;
+                    q.add(new Pos(nr,nc));
                 }
             }
             
@@ -45,7 +43,7 @@ class Solution {
     }
 }
 
-class Pos{
+class Pos {
     int r;
     int c;
     public Pos(int r,int c){
