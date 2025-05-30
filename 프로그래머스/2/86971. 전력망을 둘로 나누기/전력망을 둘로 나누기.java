4901eff51f243b548e1963ddfc51173b299f6d86
hessepark <78264208+hessepark@users.noreply.github.com>
2025-05-30 10:38:36 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 2.01 ms, Memory: 80.4 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -1,48 +1,65 @@
 import java.util.*;
 import java.io.*;
+
 class Solution {
-    public static int n;
-    public static int minDiff;
-    public static boolean isVisited[];
+    
     public static ArrayList<Integer> list[];
+    public static boolean[] isVisited;
+    
     public int solution(int n, int[][] wires) {
-        this.n=n;
-        minDiff=Integer.MAX_VALUE;
-        isVisited=new boolean[n+1];
-        list=new ArrayList[n+1];
+        int min = Integer.MAX_VALUE;
         
-        for(int i=1;i<n+1;i++){
-            list[i]=new ArrayList<>();
+        list = new ArrayList[n+1];
+        
+        
+        for(int i=1;i<=n;i++){
+            list[i] = new ArrayList<>();
         }
         
         for(int i=0;i<wires.length;i++){
-            int a = wires[i][0];
-            int b = wires[i][1];
+            int a= wires[i][0];
+            int b= wires[i][1];
+            
             list[a].add(b);
             list[b].add(a);
         }
         
-        dfs(1);
+        for(int i=0;i<wires.length;i++){
+            
+            isVisited = new boolean[n+1];
+            
+            int a= wires[i][0];
+            int b= wires[i][1];
+            
+            list[a].remove(Integer.valueOf(b));
+            list[b].remove(Integer.valueOf(a));
+            
+            int cnt = dfs(1);
+            //System.out.println(cnt);
+            
+            min=Math.min(min,Math.abs(n-cnt-cnt));
+            
+            list[a].add(b);
+            list[b].add(a);
+            
+        }
         
-        return minDiff;
+        return min;
     }
     
     public static int dfs(int num){
         
-        isVisited[num]=true;
+        isVisited[num] = true;
         
-        int count=1;
+        int cnt=0;
         
         for(int child:list[num]){
             if(!isVisited[child]){
-                count+=dfs(child);
+                cnt+=dfs(child);
             }
         }
         
-        minDiff=Math.min(minDiff,Math.abs(n-count*2));
-        
-        return count;
-        
+        return cnt+1;
         
     }
 }
