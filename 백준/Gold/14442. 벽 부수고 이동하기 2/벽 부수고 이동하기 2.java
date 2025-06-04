68dd92e7de8d2589284e1304790c7b0b985f2936
hessepark <78264208+hessepark@users.noreply.github.com>
2025-06-05 07:36:31 +0900

    [Gold III] Title: 벽 부수고 이동하기 2, Time: 1584 ms, Memory: 370116 KB -BaekjoonHub

--- 백준/Gold/14442. 벽 부수고 이동하기 2/벽 부수고 이동하기 2.java
+++ 백준/Gold/14442. 벽 부수고 이동하기 2/벽 부수고 이동하기 2.java
@@ -1,75 +1,85 @@
 import java.util.ArrayDeque;
+import java.util.Arrays;
 import java.util.Scanner;
 
 class Main {
 
-	public static int dr[] = { -1, 0, 1, 0 };
-	public static int dc[] = { 0, 1, 0, -1 };
-	public static int isVisited[][][];
-	public static int maze[][];
+    public static int dr[] = {-1, 0, 1, 0};
+    public static int dc[] = {0, 1, 0, -1};
+    public static int visited[][][];
+    public static char map[][];
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
 
-		int n = sc.nextInt();
-		int m = sc.nextInt();
-		int k = sc.nextInt();
-		isVisited = new int[n + 1][m + 1][k+1];
-		maze = new int[n + 1][m + 1];
+        Scanner sc = new Scanner(System.in);
 
-		for (int i = 1; i < n + 1; i++) {
-			String str = sc.next();
-			for (int j = 1; j < m + 1; j++) {
-				maze[i][j] = str.charAt(j-1) - '0';
-			}
-		}
+        int answer = 0;
 
-		ArrayDeque<Point> q = new ArrayDeque<>();
-		q.add(new Point(1, 1, 0));
-		isVisited[1][1][0] = 1;
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int k = sc.nextInt();
+        visited = new int[n + 1][m + 1][k+1];
+        map = new char[n + 1][m + 1];
 
-		while (!q.isEmpty()) {
+        sc.nextLine();
 
-			Point now = q.poll();
-			if (now.r == n && now.c == m) {
-				System.out.println(isVisited[now.r][now.c][now.isBroken]);
-				return;
-			}
+        for (int i = 1; i < n + 1; i++) {
+            String line = sc.nextLine();
+            for (int j = 1; j < m + 1; j++) {
+                map[i][j] = line.charAt(j - 1);
+            }
+        }
 
-			for (int i = 0; i < 4; i++) {
+        ArrayDeque<Pos> q = new ArrayDeque<>();
+        visited[1][1][0] = 1;
+        q.add(new Pos(1, 1, 0));
 
-				int nr = dr[i] + now.r;
-				int nc = dc[i] + now.c;
+        while (!q.isEmpty()) {
 
-				if (nr == 0 || nc == 0 || nr == n + 1 || nc == m + 1) {
-					continue;
-				}
+            Pos now = q.poll();
 
-				if (isVisited[nr][nc][now.isBroken] == 0 && maze[nr][nc] == 0) {
-					isVisited[nr][nc][now.isBroken] = isVisited[now.r][now.c][now.isBroken] + 1;
-					q.add(new Point(nr, nc, now.isBroken));
-				} else if (now.isBroken < k&&isVisited[nr][nc][now.isBroken+1] == 0) {
-					isVisited[nr][nc][now.isBroken+1] = isVisited[now.r][now.c][now.isBroken] + 1;
-					q.add(new Point(nr, nc, now.isBroken+1));
-				}
+            if (now.r == n && now.c == m) {
+                System.out.println(visited[now.r][now.c][now.idx]);
+                return;
+            }
 
-			}
+            for (int i = 0; i < 4; i++) {
+                int nr = now.r + dr[i];
+                int nc = now.c + dc[i];
 
-		}
-		
-		System.out.println(-1);
+                if (nr <= 0 || nc <= 0 || nr > n || nc > m) {
+                    continue;
+                }
 
-	}
+                if (visited[nr][nc][now.idx] == 0) {
+                    if (map[nr][nc] == '0') {
+                        q.add(new Pos(nr, nc, now.idx));
+                        visited[nr][nc][now.idx] = visited[now.r][now.c][now.idx] + 1;
+                    } else if (map[nr][nc] == '1' && now.idx <k&& visited[nr][nc][now.idx + 1] == 0) {
+                        q.add(new Pos(nr, nc, now.idx + 1));
+                        visited[nr][nc][now.idx + 1] = visited[now.r][now.c][now.idx] + 1;
+                    }
+                }
+
+
+            }
+
+        }
+
+        System.out.println(-1);
+
+
+    }
 }
 
-class Point {
-	int r;
-	int c;
-	int isBroken;
+class Pos {
+    int r;
+    int c;
+    int idx;
 
-	public Point(int r, int c, int isBroken) {
-		this.r = r;
-		this.c = c;
-		this.isBroken = isBroken;
-	}
+    public Pos(int r, int c, int idx) {
+        this.r = r;
+        this.c = c;
+        this.idx = idx;
+    }
 }
