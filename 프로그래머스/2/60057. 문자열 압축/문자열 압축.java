83a45478f9d31bad741f4e9b7091aaefe252faea
hessepark <78264208+hessepark@users.noreply.github.com>
2025-06-06 13:56:23 +0900

    [level 2] Title: 문자열 압축, Time: 4.34 ms, Memory: 84.3 MB -BaekjoonHub

--- 프로그래머스/2/60057. 문자열 압축/문자열 압축.java
+++ 프로그래머스/2/60057. 문자열 압축/문자열 압축.java
@@ -1,56 +1,47 @@
 import java.util.*;
 import java.io.*;
-
 class Solution {
     public int solution(String s) {
-        int answer = s.length();
+        int min=s.length();
         
         for(int l=1;l<=s.length()/2;l++){
-            
-            ArrayList<String>word = new ArrayList<>();
-            
-            for(int i=0;i<s.length();i+=l){
-                word.add(s.substring(i,Math.min(s.length(),i+l)));
-            }
-            
-            ArrayDeque<Word>q = new ArrayDeque<>();
-            q.add(new Word(word.get(0),1));
-            
-            for(int i=1;i<word.size();i++){
-                
-                if(q.peekLast().word.equals(word.get(i))){
-                    Word now = q.pollLast();
-                    q.add(new Word(now.word,now.cnt+1));
-                }
-                else{
-                    q.add(new Word(word.get(i),1));
-                }
-                
-            }
-            
-            StringBuilder sb = new StringBuilder();
-            
-            for(Word alpha: q){
-                sb.append(alpha.word);
-                if(alpha.cnt>=2){
-                    sb.append(alpha.cnt);
-                }
-            }
-            
-            answer=Math.min(answer,sb.toString().length());
-            
+            min=Math.min(min,compress(s,l));
         }
         
+        return min;
+    }
+    
+    
+    public static int compress(String s, int length) {
+    
+        int count=1;
+        String prev="";
+        StringBuilder sb = new StringBuilder();
         
-        return answer;
+        for(int i=0;i<s.length();i+=length){
+            String str =s.substring(i,Math.min(s.length(),i+length));
+            if(str.equals(prev)){
+                count++;
+            }
+            else{
+                if(count>1){
+                    sb.append(count);
+                }
+                sb.append(prev);
+                prev=str;
+                count=1;
+            }
+        }
+        
+        if(count>1){
+            sb.append(count);
+        }
+        sb.append(prev);
+        
+        //System.out.println(length+"결과: "+sb.toString());
+        
+        return sb.toString().length();
+        
     }
-}
-
-class Word {
-    String word;
-    int cnt;
-    public Word(String word,int cnt){
-        this.word=word;
-        this.cnt=cnt;
-    }
+    
 }
