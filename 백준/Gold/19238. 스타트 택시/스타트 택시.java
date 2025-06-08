c567dd0b3fc0a02f8834219ac330d75831b09b31
hessepark <78264208+hessepark@users.noreply.github.com>
2025-06-08 18:02:13 +0900

    [Gold II] Title: 스타트 택시, Time: 236 ms, Memory: 28264 KB -BaekjoonHub

--- 백준/Gold/19238. 스타트 택시/스타트 택시.java
+++ 백준/Gold/19238. 스타트 택시/스타트 택시.java
@@ -1,6 +1,7 @@
 
-
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
@@ -16,6 +17,7 @@ public class Main {
         StringTokenizer st;
 
         st = new StringTokenizer(br.readLine());
+
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
         int fuel = Integer.parseInt(st.nextToken());
@@ -42,104 +44,115 @@ public class Main {
             int sC = Integer.parseInt(st.nextToken());
             int eR = Integer.parseInt(st.nextToken());
             int eC = Integer.parseInt(st.nextToken());
+
             people.add(new Person(sR, sC, eR, eC));
         }
 
-        Collections.sort(people, (o1, o2) -> {
-            if (o1.sr != o2.sr) return Integer.compare(o1.sr, o2.sr);
-            return Integer.compare(o1.sc, o2.sc);
+
+        Collections.sort(people,(o1,o2)->{
+            if(o1.sr!=o2.sr){
+                return Integer.compare(o1.sr,o2.sr);
+            }
+            else if(o1.sc!=o2.sc){
+                return Integer.compare(o1.sc,o2.sc);
+            }
+            else{
+                return 0;
+            }
         });
 
         int x = startR;
         int y = startC;
+        while(!people.isEmpty()){
 
-        while (!people.isEmpty()) {
-
-            ArrayDeque<Pos> q = new ArrayDeque<>();
-            int[][] visited = new int[n + 1][n + 1];
-            q.add(new Pos(x, y, fuel));
-            visited[x][y] = 1;
-
-            while (!q.isEmpty()) {
-                Pos now = q.poll();
-
-                for (int k = 0; k < 4; k++) {
-                    int nr = now.r + dr[k];
-                    int nc = now.c + dc[k];
-
-                    if (nr <= 0 || nc <= 0 || nr > n || nc > n || isWall[nr][nc] == 1) continue;
-
-                    if (visited[nr][nc] == 0) {
-                        visited[nr][nc] = visited[now.r][now.c] + 1;
-                        q.add(new Pos(nr, nc, now.fuel - 1));
-                    }
-                }
-            }
-
-            int dist = visited[people.get(0).sr][people.get(0).sc];
-            int minIdx = 0;
-
-            for (int i = 1; i < people.size(); i++) {
-                int curDist = visited[people.get(i).sr][people.get(i).sc];
-                if (curDist == 0) continue;
-
-                if (curDist < dist || 
-                    (curDist == dist && (
-                        people.get(i).sr < people.get(minIdx).sr || 
-                        (people.get(i).sr == people.get(minIdx).sr && people.get(i).sc < people.get(minIdx).sc)
-                    ))) {
-                    dist = curDist;
-                    minIdx = i;
-                }
-            }
-
-            if (dist == 0 || fuel - (dist - 1) < 0) {
-                System.out.println(-1);
-                return;
-            }
-
-            fuel -= (dist - 1);
-
-            visited = new int[n + 1][n + 1];
-            x = people.get(minIdx).sr;
-            y = people.get(minIdx).sc;
-
-            q = new ArrayDeque<>();
-            q.add(new Pos(x, y, fuel));
-            visited[x][y] = 1;
+            ArrayDeque<Pos>q = new ArrayDeque<>();
+            int[][]visited=new int[n+1][n+1];
+            q.add(new Pos(x,y,fuel));
+            visited[x][y]=1;
 
             boolean arrived = false;
 
-            while (!q.isEmpty()) {
-                Pos now = q.poll();
+           while(!q.isEmpty()){ //현재 기준 퍼트리기 (매번 최소 거리 찾기 위함)
 
-                if (now.r == people.get(minIdx).er && now.c == people.get(minIdx).ec) {
-                    if (now.fuel < 0) {
-                        System.out.println(-1);
-                        return;
+                Pos now= q.poll();
+
+                for(int k=0;k<4;k++){
+                    int nr=dr[k]+now.r;
+                    int nc=dc[k]+now.c;
+
+                    if(nr<=0||nc<=0||nr>n||nc>n||isWall[nr][nc]==1){
+                        continue;
                     }
-                    fuel = now.fuel + (visited[now.r][now.c] - 1) * 2;
-                    x = now.r;
-                    y = now.c;
-                    people.remove(minIdx);
-                    arrived = true;
-                    break;
-                }
 
-                if (now.fuel <= 0) continue;
-
-                for (int k = 0; k < 4; k++) {
-                    int nr = now.r + dr[k];
-                    int nc = now.c + dc[k];
-
-                    if (nr <= 0 || nc <= 0 || nr > n || nc > n || isWall[nr][nc] == 1) continue;
-
-                    if (visited[nr][nc] == 0) {
-                        visited[nr][nc] = visited[now.r][now.c] + 1;
-                        q.add(new Pos(nr, nc, now.fuel - 1));
+                    if(visited[nr][nc]==0){
+                        visited[nr][nc]=visited[now.r][now.c]+1;
+                        q.add(new Pos(nr,nc,now.fuel-1));
                     }
+
                 }
             }
+
+           //최소 거리, 인덱스 찾기
+           int dist=visited[people.get(0).sr][people.get(0).sc];
+           int minIdx=0;
+
+           for(int i=1;i<people.size();i++){
+               if(visited[people.get(i).sr][people.get(i).sc]<dist){
+                   dist=visited[people.get(i).sr][people.get(i).sc];
+                   minIdx=i;
+               }
+           }
+
+           //출발 지점 연료량 계산
+           if(fuel-(dist-1)<0||dist==0){ //데리러 못가도 return
+               System.out.println(-1);
+               return;
+           }
+           fuel -=(dist-1);
+            //System.out.println("출발지 도착:"+fuel);
+
+           //도착 지점까지 보내기
+           visited=new int[n+1][n+1];
+           x=people.get(minIdx).sr;
+           y=people.get(minIdx).sc;
+
+           q.add(new Pos(x,y,fuel));
+           visited[x][y]=1;
+
+           while(!q.isEmpty()){
+
+               Pos now = q.poll();
+
+               if(now.r==people.get(minIdx).er&&now.c==people.get(minIdx).ec){
+                   if(now.fuel<0){
+                       System.out.println(-1);
+                       return;
+                   }
+                   fuel=now.fuel+(visited[now.r][now.c]-1)*2;
+                   x=people.get(minIdx).er;
+                   y=people.get(minIdx).ec;
+                   people.remove(minIdx);
+                   //System.out.println("도착지 도착:"+fuel);
+                   arrived = true;
+                   break;
+               }
+
+               for(int k=0;k<4;k++){
+                   int nr= now.r+dr[k];
+                   int nc= now.c+dc[k];
+
+                   if(nr<=0||nc<=0||nr>n||nc>n||isWall[nr][nc]==1){
+                       continue;
+                   }
+
+                   if(visited[nr][nc]==0){
+                       q.add(new Pos(nr,nc,now.fuel-1));
+                       visited[nr][nc]=visited[now.r][now.c]+1;
+                   }
+               }
+
+           }
+
 
             if (!arrived) {
                 System.out.println(-1);
@@ -148,6 +161,8 @@ public class Main {
         }
 
         System.out.println(fuel);
+
+
     }
 }
 
@@ -169,7 +184,7 @@ class Person {
     int er;
     int ec;
 
-    public Person(int sr, int sc, int er, int ec) {
+    Person(int sr, int sc, int er, int ec) {
         this.sr = sr;
         this.sc = sc;
         this.er = er;
