539c89f398df8cc393673d5a38724a0e6c8559c0
hessepark <78264208+hessepark@users.noreply.github.com>
2025-10-19 15:45:34 +0900

    [Silver I] Title: 용돈 관리, Time: 792 ms, Memory: 101412 KB -BaekjoonHub

--- 백준/Silver/6236. 용돈 관리/용돈 관리.java
+++ 백준/Silver/6236. 용돈 관리/용돈 관리.java
@@ -1,78 +1,70 @@
-import java.io.*;
 import java.util.Arrays;
-import java.util.HashSet;
 import java.util.Scanner;
-import java.util.StringTokenizer;
 
-public class Main {
+class Main{
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		int m = sc.nextInt();
+		int max  = Integer.MIN_VALUE;
+		
+		int arr[]=new int[n];
+		
+		for(int i=0;i<n;i++) {
+			arr[i] = sc.nextInt();
+			max=Math.max(max, arr[i]);
+		}
+		
+		long k = binarySearch(arr,m,max);
+		
+		System.out.println(k);
+		
+	}
+	
+	public static long binarySearch(int[] arr, int m, int max) {
+		
+		long l=max;
+		long r=10000000000L;
+		long ans=-1;
+		
+		while(l<=r) {
+			long mid =(l+r)/2;
+			long tmp = mid;
+			int cnt=1;
+			
+			for(int i=0;i<arr.length;i++) {
+				if(tmp-arr[i]<0) {
+					tmp=mid;
+					cnt++;
+				}
+				tmp-=arr[i];
+			}
+			
+			
+			if(cnt>m) {
+				l=mid+1;
+			}
+			else {
+				ans=mid;
+				r=mid-1;
+			}
+			
+			
+			//System.out.println("cnt: " + cnt);
+			//System.out.println("l: "+l+" r: "+r);
+			
+		}
+		
+		return ans;
+		
+		
+	}
+	
 
-    public static void main(String[] args) throws IOException {
-
-        Scanner sc = new Scanner(System.in);
-
-        int n = sc.nextInt();
-        int m = sc.nextInt();
-
-        int arr[] = new int[n];
-
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = sc.nextInt();
-        }
-
-        long k = findK(arr, m);
-
-        System.out.println(k);
-
-
-    }
-
-    public static long findK(int[] arr, int m) {
-
-        long l = Arrays.stream(arr).max().getAsInt();;
-        long r = 10000000000L;
-        long ans = -1;
-
-        while (l <= r) {
-            long mid = (l + r) / 2;
-           //System.out.println("l출력: " + l + ", r출력: " + r);
-
-            //System.out.println("mid출력: " + mid);
-            long tmp = mid;
-
-            int cnt = 1;
-
-
-            for (int i = 0; i < arr.length; i++) {
-
-                if (mid - arr[i] < 0) {
-                    cnt++;
-                    mid = tmp;
-                }
-                mid -= arr[i];
-            }
-
-            //System.out.println("cnt 출력:" + cnt + ", mid 출력: " + tmp);
-
-            if (cnt > m) {
-                l = tmp + 1;
-            } else {
-                ans = tmp;
-                r = tmp - 1;
-            }
-
-        }
-
-        return ans;
-
-    }
-
-    //숫자가 더 커지면 늘이고,
-    //숫자가 같거나 작으면 갱신 후 줄이기
-
-
+	
+	
 }
-
-
-
-
 
