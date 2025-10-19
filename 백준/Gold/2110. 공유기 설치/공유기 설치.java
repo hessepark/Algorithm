837ed363356ea70854d81ccb8c48c400e35d4c29
hessepark <78264208+hessepark@users.noreply.github.com>
2025-10-19 17:52:28 +0900

    [Gold IV] Title: 공유기 설치, Time: 952 ms, Memory: 176124 KB -BaekjoonHub

--- 백준/Gold/2110. 공유기 설치/공유기 설치.java
+++ 백준/Gold/2110. 공유기 설치/공유기 설치.java
@@ -3,54 +3,71 @@ import java.util.Scanner;
 
 class Main {
 
-	public static int calculateCnt(int[] arr, int m) {
+	public static void main(String[] args) {
 
-		int cnt = 1;
-		int pastX = arr[0];
+		Scanner sc = new Scanner(System.in);
 
-		for (int i = 1; i < arr.length; i++) {
-			if (arr[i] - pastX >= m) {
-				pastX = arr[i];
+		int n = sc.nextInt();
+		int c = sc.nextInt();
+		
+		int arr[]=new int[n];
+		
+		for(int i=0;i<n;i++) {
+			arr[i] = sc.nextInt();
+		}
+		
+		Arrays.sort(arr);
+		
+		int result = find(arr,c);
+		
+		System.out.print(result);
+	}
+	
+	public static int find(int[] arr, int c) {
+		
+		int l =1;
+		int r=arr[arr.length-1]-arr[0];
+		int ans=-1;
+		
+		while(l<=r) {
+			int m = (l+r)/2;
+			
+			if(isPossible(arr,m)>=c) {
+				l=m+1;
+				ans = m;
+			}
+			else {
+				r=m-1;
+			}
+			
+		}
+		
+		
+		return ans;
+		
+		
+	}
+	
+	public static int isPossible(int[] arr,int m) {
+		
+		int cnt=1;
+		int pastX=arr[0];
+		
+		for(int i=1;i<arr.length;i++) {
+			if(arr[i]-pastX>=m) {
+				pastX=arr[i];
 				cnt++;
 			}
 		}
-
+		
 		return cnt;
-
+		
 	}
+	
+	
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+	// 공유기 개수 같거나 많으면 거리 늘이기, 아니면 줄이기
 
-		int N = sc.nextInt();
-		int C = sc.nextInt();
 
-		int arr[] = new int[N];
 
-		for (int i = 0; i < N; i++) {
-			arr[i] = sc.nextInt();
-		}
-
-		Arrays.sort(arr);
-
-		int l = 1;
-		int r = arr[arr.length - 1]-arr[0];
-		int ans = -1;
-
-		while (l <= r) {
-			int m = (l + r) / 2;
-
-			if (calculateCnt(arr, m) >= C) {
-				ans = m;
-				l = m + 1;
-			}
-
-			else {
-				r = m - 1;
-			}
-
-		}
-		System.out.println(ans);
-
-	}
 }
