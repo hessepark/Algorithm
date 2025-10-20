ed0946c339c22c50507c378aa9b476c9a362cf0d
hessepark <78264208+hessepark@users.noreply.github.com>
2025-10-20 14:11:28 +0900

    [Silver II] Title: 랜선 자르기, Time: 360 ms, Memory: 30528 KB -BaekjoonHub

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
@@ -1,60 +1,60 @@
 import java.util.Scanner;
 
-class Main{
-	
+class Main {
 	public static void main(String[] args) {
-		
 		Scanner sc = new Scanner(System.in);
 		
 		int k = sc.nextInt();
 		int n = sc.nextInt();
 		
-		int arr[]=new int[k];
+		int arr[] = new int[k];
 		
-		for(int i=0;i<k;i++) {
+		for(int i=0;i<arr.length;i++) {
 			arr[i] = sc.nextInt();
 		}
 		
-		long maxLength = caculate(arr,n);
+		long length = find(arr, n);
 		
-		System.out.println(maxLength);
+		System.out.println(length);
 	}
 	
-	public static long caculate(int[] arr,int target) {
+	public static long find(int[] arr, int n) {
 		
 		long l=0;
-		long r=(1L<<32)-1;
-		long length=-1;
+		long r=Integer.MAX_VALUE;
+		long ans=-1;
 		
 		while(l<=r) {
-			long m = (l+r)/2;
+			long mid=(l+r)/2;
 			
-			if(cut(arr,m)>=target) {
-				l=m+1;
-				length=m;
+			//System.out.println("l 출력: "+l+" r 출력: " + r);
+			
+			if(count(arr,mid)>=n) {
+				l=mid+1;
+				ans=mid;
 			}
 			else {
-				r=m-1;
+				r=mid-1;
 			}
 		}
 		
-			return length;
-		
+		return ans;
 	}
 	
-	public static long cut(int[] arr,long m) {
+	public static long count(int[] arr,long mid) {
 		
-		long sum = 0;
+		long sum=0;
 		
 		for(int i=0;i<arr.length;i++) {
-			sum+=arr[i]/m;
+			sum+=arr[i]/mid;
 		}
+		
+		//System.out.println("mid 출력: "+mid);
+		//System.out.println("개수 출력: "+sum);
 		
 		return sum;
 		
-		
 	}
 	
-	
-}
 
+}
