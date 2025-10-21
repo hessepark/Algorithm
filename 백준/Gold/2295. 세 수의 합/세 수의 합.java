d9b63e94c184b31bb84bdea95733a0567b334883
hessepark <78264208+hessepark@users.noreply.github.com>
2025-10-21 12:23:09 +0900

    [Gold IV] Title: 세 수의 합, Time: 476 ms, Memory: 61028 KB -BaekjoonHub

--- 백준/Gold/2295. 세 수의 합/세 수의 합.java
+++ 백준/Gold/2295. 세 수의 합/세 수의 합.java
@@ -1,51 +1,38 @@
-import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Scanner;
 
-public class Main {
-
-    public static void main(String[] args) {
-
-        Scanner sc = new Scanner(System.in);
-
-        HashSet<Integer> set = new HashSet<>();
-
-        int n = sc.nextInt();
-
-        int arr[]=new int[n];
-
-        for(int i=0;i<n;i++){
-            arr[i] = sc.nextInt();
-        }
-
-        for(int i=0;i<n;i++){
-            for(int j=i;j<n;j++){
-                set.add(arr[i]+arr[j]);
-            }
-        }
-
-        //System.out.println(set);
-
-        int ans=-1;
-
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                if(set.contains(arr[i]-arr[j])){
-                    ans=Math.max(ans,arr[i]);
-                }
-            }
-        }
-
-        System.out.println(ans);
-
-
-    }
-
-
+class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		
+		int arr[]=new int[n];
+		
+		for(int i=0;i<arr.length;i++) {
+			arr[i] = sc.nextInt();
+		}
+		
+		HashSet<Integer> set = new HashSet<>();
+		
+		for(int i=0;i<n;i++) {
+			for(int j=i;j<n;j++) {
+				set.add(arr[i]+arr[j]);
+			}
+		}
+		
+		int max =Integer.MIN_VALUE;
+		
+		for(int i=0;i<n;i++) {
+			for(int j=i;j<n;j++) {
+				if(set.contains(arr[j]-arr[i])) {
+					max=Math.max(max, arr[j]);
+				}
+			}
+		}
+		
+		System.out.println(max);
+		
+	}
+		
 
 }
-
-
-
-
-
