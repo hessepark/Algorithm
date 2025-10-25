962c44bf03c5698022794125438bfdec80134814
hessepark <78264208+hessepark@users.noreply.github.com>
2025-10-25 21:53:08 +0900

    [Bronze III] Title: 소금 폭탄, Time: 172 ms, Memory: 17628 KB -BaekjoonHub

---
+++ 백준/Bronze/13223. 소금 폭탄/소금 폭탄.java
@@ -0,0 +1,40 @@
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Scanner;
+
+class Main {
+	public static void main(String[] args) throws IOException {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		String time1[]=sc.next().split(":");
+		String time2[]=sc.next().split(":");
+		
+		int timeSum1=Integer.parseInt(time1[0])*3600+Integer.parseInt(time1[1])*60+Integer.parseInt(time1[2]);
+		int timeSum2=Integer.parseInt(time2[0])*3600+Integer.parseInt(time2[1])*60+Integer.parseInt(time2[2]);
+		
+		
+		if(timeSum2==timeSum1) {
+			System.out.println("24:00:00");
+			return;
+		}
+		else if(timeSum2<timeSum1) {
+			timeSum2+=24*3600;
+		}
+		
+		int ans=timeSum2-timeSum1;
+		
+		int t=ans/3600;
+		int m=ans%3600/60;
+		int s=ans%60;
+		
+		//System.out.println(t+" "+m+" "+s);
+		
+		System.out.printf("%02d:%02d:%02d",t,m,s);
+		
+
+	}
+	
+
+
+}
