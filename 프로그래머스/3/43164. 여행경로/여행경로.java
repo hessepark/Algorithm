deed498ce8911038091c16a606a1b5c9d098eea8
hessepark <78264208+hessepark@users.noreply.github.com>
2025-10-27 21:44:40 +0900

    [level 3] Title: 여행경로, Time: 0.76 ms, Memory: 77.3 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.java
+++ 프로그래머스/3/43164. 여행경로/여행경로.java
@@ -3,47 +3,51 @@ import java.io.*;
 
 class Solution {
     
-    public static boolean isVisited[];
     public static ArrayList<String>list = new ArrayList<>();
     
     public String[] solution(String[][] tickets) {
-        String[] answer = {};
+        String[] answer = new String[tickets.length];
         
-        isVisited=new boolean[tickets.length];
-        
-        Arrays.sort(tickets,(o1,o2)->{
+        Arrays.sort(tickets,(o1,o2)-> {
             if(o1[0].compareTo(o2[0])==0){
-                return o1[1].compareTo(o2[1]);
-            }
-            else{
+                return o1[1].compareTo(o2[1]);   
+            } else{
                 return o1[0].compareTo(o2[0]);
             }
         });
         
+        boolean isVisited[]=new boolean[tickets.length];
         list.add("ICN");
-        dfs("ICN",tickets);
+        dfs("ICN",isVisited,tickets);
         
-        return list.toArray(new String[0]);
-    }
-    
-    public static void dfs(String cur,String[][] tickets){
+        answer = new String[list.size()];
         
-        if(list.size()==tickets.length+1){
-            return;
+        for(int i=0;i<list.size();i++){
+            answer[i]=list.get(i);
         }
         
+        return answer;
+    }
+    
+    public static void dfs(String word,boolean[] isVisited,String[][] tickets) {
+        
+        
+        
         for(int i=0;i<tickets.length;i++){
-            if(!isVisited[i]&&tickets[i][0].equals(cur)){
-                list.add(tickets[i][1]);
-                isVisited[i]=true;
-                dfs(tickets[i][1],tickets);
-                if(list.size()==tickets.length+1){
-                    return;
+            if(!isVisited[i]){
+                if(tickets[i][0].equals(word)){
+                    list.add(tickets[i][1]);
+                    isVisited[i]=true;
+                    dfs(tickets[i][1],isVisited,tickets);
+                    if(list.size()==tickets.length+1){
+                        return;
+                    }
+                    list.remove(list.size()-1);
+                    isVisited[i]=false;
                 }
-                isVisited[i]=false;
-                list.remove(list.size()-1);
             }
         }
         
     }
+    
 }
