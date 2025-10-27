dce039fe6cac369ffbabe2e014b29616a6f88baa
hessepark <78264208+hessepark@users.noreply.github.com>
2025-10-27 22:07:38 +0900

    [level 2] Title: 소수 찾기, Time: 4.22 ms, Memory: 86.4 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/README.md
+++ 프로그래머스/2/42839. 소수 찾기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 소수 찾기 - 42839 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42839) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42839?language=java) 
 
 ### 성능 요약
 
-메모리: 75.2 MB, 시간: 4.05 ms
+메모리: 86.4 MB, 시간: 4.22 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 01월 29일 23:33:10
+2025년 10월 27일 22:07:36
 
 ### 문제 설명
 
--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -1,61 +1,51 @@
-import java.util.HashSet;
+import java.util.*;
+import java.io.*;
 
 class Solution {
-
-	// 시작점에서부터 다 본다.
-
-	public static HashSet<Integer> set = new HashSet<>();
-	public static boolean isVisited[];
-	public static int ans;
-
-	public int solution(String numbers) {
-		ans = 0;
-
-		isVisited = new boolean[numbers.length()];
-
-		dfs(0,0, numbers);
-
-		return ans;
-	}
-
-	public static void dfs(int num,int depth,String numbers) {
-
-		//System.out.println(cur);
-
-		if (isPrime(num)) {
-			set.add(num);
-			ans++;
-		}
+    
+    public static HashSet<Integer>set;
+    
+    public int solution(String numbers) {
+        set = new HashSet<>();
+        
+        boolean isVisited[]=new boolean[numbers.length()];
+        
+        dfs(0,0,isVisited,numbers);
+        
+        return set.size();
+    }
+    
+    public static void dfs(int num, int depth, boolean[] isVisited, String numbers) {
+        
+        if(isPrime(num)&&!set.contains(num)){
+            set.add(num);
+        }
         
         if(depth==numbers.length()){
             return;
         }
-
-		for (int i = 0; i < numbers.length(); i++) {
-			if (!isVisited[i]) {
-				isVisited[i] = true;
-				dfs(num*10+numbers.charAt(i)-'0',depth+1,numbers);
-				//System.out.println("종료: " + numbers.charAt(i) + "종료 당시 cur:" + cur);
-				isVisited[i] = false;
-			}
-			// isVisited[i]=false; 여기 들어가면 121 나온다. 이유는 1이 안 간 거 처리 돼버림
-		}
-
-	}
-
-	public static boolean isPrime(int num) {
-
-		if (num<=1||set.contains(num)) {
-			return false;
-		}
-
-		for (int i = 2; i <= Math.sqrt(num); i++) {
-			if (num % i == 0) {
-				return false;
-			}
-		}
-
-		return true;
-
-	}
+        
+        for(int i=0;i<numbers.length();i++) {
+            if(!isVisited[i]){
+                isVisited[i]=true;
+                dfs(num*10+numbers.charAt(i)-'0',depth+1,isVisited,numbers);
+                isVisited[i]=false;
+            }
+        }
+        
+    }
+    
+    public static boolean isPrime(int num){
+        
+        if(num<=1) return false;
+        
+        for(int i=2;i<=Math.sqrt(num);i++) {
+            if(num%i==0){
+                return false;
+            }
+        }
+        
+        return true;
+        
+    }
 }
