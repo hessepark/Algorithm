89af64097a5f93aa85951385a88ba0e4e60951d4
hessepark <78264208+hessepark@users.noreply.github.com>
2025-11-10 18:40:55 +0900

    [Gold V] Title: 수 고르기, Time: 904 ms, Memory: 111612 KB -BaekjoonHub

--- 백준/Gold/2230. 수 고르기/수 고르기.java
+++ 백준/Gold/2230. 수 고르기/수 고르기.java
@@ -2,38 +2,44 @@ import java.io.IOException;
 import java.util.Arrays;
 import java.util.Scanner;
 
-class Main {
-	public static void main(String[] args) throws IOException {
-		
-		Scanner sc = new Scanner(System.in);
-		
-		int n = sc.nextInt();
-		int m = sc.nextInt();
-		
-		int arr[]=new int[n];
-		
-		for(int i=0;i<n;i++) {
-			arr[i] = sc.nextInt();
-		}
-		
-		Arrays.sort(arr);
-		
-		int r=0;
-		int diff=Integer.MAX_VALUE;
-		
-		for(int i=0;i<n;i++) {
-			while(arr[r]-arr[i]<m&&r<n-1) {
-				r++;
-			}
-			if(arr[r]-arr[i]>=m) {
-				diff=Math.min(diff, arr[r]-arr[i]);
-			}
-		}
-		
-		System.out.println(diff);
-		
-	
-	}
-	
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        int arr[]=new int[n];
+
+        for(int i=0;i<n;i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        Arrays.sort(arr);
+
+        int minDiff=Integer.MAX_VALUE;
+        int idx=0;
+
+        for(int i=0;i<n;i++) {
+
+            while(idx<n-1&&arr[idx]-arr[i]<m){
+                idx++;
+            }
+
+            if(arr[idx]-arr[i]>=m)
+            minDiff=Math.min(minDiff,arr[idx]-arr[i]);
+
+        }
+
+        System.out.println(minDiff);
+
+    }
 
 }
+
+
+
+
+
