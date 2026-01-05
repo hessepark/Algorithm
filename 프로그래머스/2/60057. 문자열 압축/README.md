ab2d2ca83d1326b6b3be8f01de30c6f2c86d55cc
hessepark <78264208+hessepark@users.noreply.github.com>
2026-01-05 22:35:30 +0900

    [level 2] Title: 문자열 압축, Time: 7.52 ms, Memory: 95.2 MB -BaekjoonHub

--- 프로그래머스/2/60057. 문자열 압축/README.md
+++ 프로그래머스/2/60057. 문자열 압축/README.md
@@ -1,10 +1,10 @@
 # [level 2] 문자열 압축 - 60057 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/60057?language=java&gad_source=1&gad_campaignid=22199869887&gbraid=0AAAAAC_c4nDwEYWah2HJuHZJEvV7v3YrN&gclid=CjwKCAjw04HIBhB8EiwA8jGNbfvV7XtGxv5bv8oIpXBHVFIKj7-MKmK4Y-fc_ZSwxXTXlPin4bpl_hoCWjcQAvD_BwE) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/60057?gad_source=1&gclid=CjwKCAjw--K_BhB5EiwAuwYoyvumYgAjK-WDH3khaNwB07rFJYT6uszQiZhdRER0Wn4fdAHbOztTKBoCrL8QAvD_BwE) 
 
 ### 성능 요약
 
-메모리: 90 MB, 시간: 6.53 ms
+메모리: 95.2 MB, 시간: 7.52 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 10월 28일 22:36:27
+2026년 01월 05일 22:35:29
 
 ### 문제 설명
 
--- 프로그래머스/2/60057. 문자열 압축/문자열 압축.java
+++ 프로그래머스/2/60057. 문자열 압축/문자열 압축.java
@@ -5,61 +5,63 @@ class Solution {
     public int solution(String s) {
         int answer = Integer.MAX_VALUE;
         
-        for(int i=1;i<=s.length()/2;i++){
-            answer=Math.min(answer,zip(s,i));
-        }
-        
         if(s.length()==1){
             return 1;
+        }
+        
+        for(int i=1;i<=s.length()/2;i++) {
+            answer=Math.min(answer, compress(s,i));
         }
         
         return answer;
     }
     
-    public static int zip(String s, int l) {
+    
+    public int compress(String s, int l) {
         
-        ArrayDeque<Word>q = new ArrayDeque<>();
-        
-        for(int i=0;i<s.length();i+=l) {
-            String alpha=s.substring(i,Math.min(i+l,s.length()));
-            
-            if(q.isEmpty()){
-                q.addLast(new Word(alpha,1));
-            }
-            else{
-                if(q.peekLast().word.equals(alpha)){
-                    Word now=q.pollLast();
-                    q.addLast(new Word(now.word, now.cnt+1)); //alpha해도 됨
-                }
-                else{
-                    q.addLast(new Word(alpha,1));
-                }
-            }
-            
-        }
-        
+        ArrayDeque<Alpha>q = new ArrayDeque<>();
         StringBuilder sb = new StringBuilder();
         
-        while(!q.isEmpty()){
-            Word now = q.pollLast();
-            if(now.cnt>=2){
-                sb.append(now.cnt);
+        for(int i=0;i<s.length();i+=l){
+            String sub=s.substring(i,Math.min(s.length(),i+l));
+            
+            if(q.isEmpty()){
+                q.addLast(new Alpha(1,sub));
             }
-            sb.append(now.word);
+            else {
+                Alpha next=q.peekLast();
+                if(q.peekLast().a.equals(sub)){
+                    q.pollLast();
+                    q.addLast(new Alpha(next.cnt+1,next.a));
+                }
+                else{
+                    q.addLast(new Alpha(1,sub));
+                }
+            }
         }
         
-        return sb.toString().length();
+        while(!q.isEmpty()) {
+            Alpha sub=q.pollFirst();
+            if(sub.cnt>=2){
+                sb.append(sub.cnt);
+            }
+            sb.append(sub.a);
+        }
+        
+        //System.out.println(sb);
+        
+        return sb.length();
         
     }
     
 }
 
-class Word {
-    String word;
+class Alpha {
     int cnt;
+    String a;
     
-    public Word(String word, int cnt) {
-        this.word=word;
+    public Alpha(int cnt, String a) {
         this.cnt=cnt;
+        this.a=a;
     }
 }
