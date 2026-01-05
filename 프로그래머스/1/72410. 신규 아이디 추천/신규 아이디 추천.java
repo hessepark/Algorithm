d4bbca7335efb0073d851abd2580bb5d274f4154
hessepark <78264208+hessepark@users.noreply.github.com>
2026-01-05 21:56:17 +0900

    [level 1] Title: 신규 아이디 추천, Time: 12.68 ms, Memory: 87.2 MB -BaekjoonHub

--- 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.java
+++ 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.java
@@ -3,71 +3,73 @@ import java.io.*;
 
 class Solution {
     public String solution(String new_id) {
+        String answer = "";
         
-        //1
+        // 1단계
         new_id=new_id.toLowerCase();
-        
-        
-        //2
         StringBuilder sb = new StringBuilder();
-        for(char c:new_id.toCharArray()){
-            if(c>='a'&&c<='z'||c>='0'&&c<='9'||c=='-'||c=='_'||c=='.'){
+        
+        // 2단계
+        for(int i=0;i<new_id.length();i++) {
+            char c= new_id.charAt(i);
+            if(c>='a'&&c<='z' || c>='0'&&c<='9' || c=='-' || c=='_' || c=='.') {
                 sb.append(c);
             }
         }
         
         new_id=sb.toString();
-        sb=new StringBuilder();
+        sb=new StringBuilder(); //setLength 0 도 가능?
         
-        boolean isPeriod=false;
-        for(int i=0;i<new_id.length();i++){
-            char c =new_id.charAt(i);
-            if(c=='.'){
+        
+        // 3단계
+        boolean isPeriod = false;
+        for(int i=0;i<new_id.length();i++) {
+            if(new_id.charAt(i)=='.') {
                 isPeriod=true;
             }
             else{
                 if(isPeriod){
                     sb.append('.');
                 }
-                sb.append(c);
-                isPeriod=false;
+                sb.append(new_id.charAt(i));
+                isPeriod=false; // 안으로 넣어도 될듯
             }
         }
         
+        
+        //4단계
         new_id=sb.toString();
-        sb=new StringBuilder();
         
-        
-        //4
         if(new_id.startsWith(".")){
             new_id=new_id.substring(1,new_id.length());
         }
         
         if(new_id.endsWith(".")){
-            new_id=new_id.substring(0,new_id.length()-1);
+            new_id=new_id.substring(0, new_id.length()-1);
         }
         
-        //5
+        //5단계
         if(new_id.length()==0){
-            new_id+="a";
+            new_id="a";
         }
         
-        //6
-        if(new_id.length()>=16){
+        //6단계
+        if(new_id.length()>=16) {
             new_id=new_id.substring(0,15);
-                 if(new_id.endsWith(".")){
-            new_id=new_id.substring(0,new_id.length()-1);
-        }
+            
+            if(new_id.endsWith(".")){
+                new_id=new_id.substring(0,new_id.length()-1);
+            }
         }
         
-        //7
         char c = new_id.charAt(new_id.length()-1);
         while(new_id.length()<=2){
             new_id+=c;
         }
         
+       
+        
         
         return new_id;
-        
     }
 }
